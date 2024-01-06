389b12f7a870ede183f9fded9485396d31d24d3f
K-Musty <kmustapha9564@gmail.com>
2024-01-06 14:59:15 -0800

    day 1 task 2

---
+++ 01-day/day-1-2-exercise.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+ print('e.g. print("Hello" + "Wworld")')
+print("New lines can be created with a backlash and n.")
