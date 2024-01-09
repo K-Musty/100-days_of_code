c3933dae5639eae16d8a67c5b84a291eabef6b9e
K-Musty <kmustapha9564@gmail.com>
2024-01-08 17:02:05 -0800

    commit for day one project

---
+++ 01-day/band-name-generator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Hello!!")
+print("Welcome to Band-name generator")
+
+city = input("What city where you born?\n")
+pet_name = input("What is the name of your pet or favourite animal?\n")
+
+print(city + pet_name)
