0fb94bb6aa92d08f0d389300916ed6be34c8c766
K-Musty <kmustapha9564@gmail.com>
2024-01-08 17:06:43 -0800

    Day one project

--- 01-day/band-name-generator.py
+++ 01-day/band-name-generator.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 print("Hello!!")
-print("Welcome to Band-name generator")
+print("Welcome to Band-name generator\n")
 
 city = input("What city where you born?\n")
 pet_name = input("What is the name of your pet or favourite animal?\n")
 
-print(city + pet_name)
+print("Your band name can be " + city + '-' + pet_name)
