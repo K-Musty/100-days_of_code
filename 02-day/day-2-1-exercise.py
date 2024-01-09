25363ce786363fa74b75ed3b4d1105ef922fba96
K-Musty <kmustapha9564@gmail.com>
2024-01-09 15:08:11 -0800

    commit to push task 1 day 2

--- 02-day/day-2-1-exercise.py
+++ 02-day/day-2-1-exercise.py
@@ -2,7 +2,7 @@
 print("Even and Odd checker")
 number = int(input("Which number do you want to check?\n"))
 
-if number % 2 = 0:
+if number % 2 == 0:
     print("Number is even")
 else:
     print("Number is Odd")
