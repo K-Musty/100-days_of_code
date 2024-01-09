31b3686d8c22a13b7cc645b8c7d8b1047c7b5674
K-Musty <kmustapha9564@gmail.com>
2024-01-09 15:06:34 -0800

    Ths commit would push exercise 1

---
+++ 02-day/day-2-1-exercise.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Even and Odd checker")
+number = int(input("Which number do you want to check?\n"))
+
+if number % 2 = 0:
+    print("Number is even")
+else:
+    print("Number is Odd")
