2fb127440d91f79e6e2e591e5e7f5afdd228004d
K-Musty <kmustapha9564@gmail.com>
2024-01-09 15:34:09 -0800

    day 2 task 2

---
+++ 02-day/day-2-2-exercise.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print("Program adds values in a Two digit number")
+two_digit_number = input("Type a two digit number: \n")
+
+print(str(two_digit_number[0]) + str(two_digit_number[1]))
