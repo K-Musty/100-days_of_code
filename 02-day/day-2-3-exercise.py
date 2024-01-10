9767e6c255b05bd03c2b43fd38e156f82e6eab7c
K-Musty <kmustapha9564@gmail.com>
2024-01-10 15:58:37 -0800

    commit message for task 2 exercise 3

---
+++ 02-day/day-2-3-exercise.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+print("Body Mass Calculator!")
+height = input("Enter your height in m: ")
+weight = input("Entter your weight in kg: ")
+
+body_mass_index = weight / (height * height)
+print(int(bogy_mass_index))
