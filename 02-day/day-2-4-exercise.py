d33c6def0f5c10260caecbdce5ceb497fda618b7
K-Musty <kmustapha9564@gmail.com>
2024-01-11 14:44:29 -0800

    day 2 task 4

--- 02-day/day-2-4-exercise.py
+++ 02-day/day-2-4-exercise.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-print("Day, Week and Month Calculator!")
+print("Day, Week and Month Calculator! ")
 
-age = int(input("What is your current age?"))
+age = int(input("What is your current age? "))
 
 years_left = 90 - age
 day = years_left * 365
 week = years_left * 52
 month = years_left * 12
-print("You have {day}, {week} weeks and {month} months left")
+print(f"You have {day}, {week} weeks and {month} months left")
