6fbf7024d427928e95101f546232dee4e6fd40d2
K-Musty <kmustapha9564@gmail.com>
2024-01-11 15:56:26 -0800

    commit to day 2 task 5

--- 02-day/day-2-5-exercise.py
+++ 02-day/day-2-5-exercise.py
@@ -10,6 +10,13 @@ name2 = input_name2.lower()
 name1_true = name1.count("t") + name1.count("r") + name1.count("u") + name1.count("e")
 name2_love = name2.count("l") + name2.count("o") + name2.count("v") + name2.count("e")
 
-print(str(name1_true) + str(name2_love) + "%")
+real_score = (str(name1_true) + str(name2_love))
+score = int(real_score)
+if score < 10 and score > 90:
+    print(f"Your score is {score}, you go together like coke and menthos")
+elif score  >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright together")
+else:
+    print(f"Your score is {score}")
 
 
