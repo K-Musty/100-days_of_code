a2224862964f89cbfa33ef624af8da77cc2856b7
K-Musty <kmustapha9564@gmail.com>
2024-01-10 16:05:06 -0800

    commit to push day 2 task 3

--- 02-day/day-2-3-exercise.py
+++ 02-day/day-2-3-exercise.py
@@ -3,5 +3,6 @@ print("Body Mass Calculator!")
 height = input("Enter your height in m: ")
 weight = input("Entter your weight in kg: ")
 
-body_mass_index = weight / (height * height)
-print(int(bogy_mass_index))
+body_mass_index = int(weight) / float(height) ** 2
+bmi = (int(body_mass_index))
+print(bmi)
