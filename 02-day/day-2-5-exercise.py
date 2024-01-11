218b7a1959876ded9b5644065896df696abc3d64
K-Musty <kmustapha9564@gmail.com>
2024-01-11 15:23:16 -0800

    commit day 2 task 5

---
+++ 02-day/day-2-5-exercise.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+print("Welcome to Love Calculator!")
+
+name1 = input("What is your name? \n")
+name2 = input("What is thier name? \n")
+
+lower_name1 = name1.lower()
+lower_name2 = name2.lower()
+
+print(lower_name1.count("a"))
+
+
