45dca19c692095af1659256c74d915e21e529a6e
K-Musty <kmustapha9564@gmail.com>
2024-01-14 11:33:50 -0800

    tip calculator

---
+++ 02-day/tip-calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+print("Welcome to the tip calculator!")
+total_bill = input("What was the total bill? ")
+tip_percentage = input("What percentage tip would you like to give? 10, 12, or 15? ")
+people_to_split = input("How many people to split the bill? ")
+
+if tip_percentage == 12:
+    
