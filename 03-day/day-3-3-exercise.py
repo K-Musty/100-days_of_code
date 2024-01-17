bd0c0a74aa378f82fab67718635e30ab57c56782
K-Musty <kmustapha9564@gmail.com>
2024-01-17 11:39:42 -0800

    day 3 task 3

--- 03-day/day-3-3-exercise.py
+++ 03-day/day-3-3-exercise.py
@@ -4,14 +4,12 @@
 print("Leap year checker!")
 year = int(input("Which year do you want to check? "))
 
-if (year / 4) == 0:
-    {
-    if year / 100 != 0:
+if year % 4 == 0:
+    if year % 100 != 0:
         print("This is a Leap year")
-    elif year / 400 == 0:
+    elif year % 400 == 0:
         print("This is a Leap year")
     else:
         print("This is not a Leap year")
-        }
 else:
     print("This is not a Leap year")
