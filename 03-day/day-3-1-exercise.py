37b0b857a8801a9485e0163548191b1a3ca7632d
K-Musty <kmustapha9564@gmail.com>
2024-01-17 06:34:33 -0800

    commit for minor correction

--- 03-day/day-3-1-exercise.py
+++ 03-day/day-3-1-exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 #checking wheather number is even or odd
 
-number = int(input("Please input number"))
+number = int(input("Please input number  "))
 
 if number % 2 == 0:
     print("This number is an even number")
