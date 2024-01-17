4070d51501997bc78901f308aede6cf9de8ef27d
K-Musty <kmustapha9564@gmail.com>
2024-01-17 10:24:46 -0800

    commit to push task 2

---
+++ 03-day/day-3-2-exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#Body Mass Calculator 2.0
+
+print("Body Mass Calculator!")
+height = float(input("Enter your height in m: "))
+weight = float(input("Entter your weight in kg: "))
+
+body_mass_index = float(weight) / float(height) ** 2
+bmi = (float(body_mass_index))
+if bmi <= 18.5:
+    print("You are underweight")
+elif bmi > 18.5 and <= 25:
+    print("You have a normal weight")
+elif bmi > 25 and <= 30:
+    print("You are overweight")
+elif bmi > 30 and <= 35:
+    print("You are Obese")
+else:
+    print("You are clinically Obese")
