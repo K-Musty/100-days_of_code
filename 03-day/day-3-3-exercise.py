8fecc8c2f8b4754642145a20ae4756b570902118
K-Musty <kmustapha9564@gmail.com>
2024-01-17 11:33:59 -0800

    commit to push task 3

---
+++ 03-day/day-3-3-exercise.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#Leap year calculator
+
+print("Leap year checker!")
+year = int(input("Which year do you want to check? "))
+
+if (year / 4) == 0:
+    {
+    if year / 100 != 0:
+        print("This is a Leap year")
+    elif year / 400 == 0:
+        print("This is a Leap year")
+    else:
+        print("This is not a Leap year")
+        }
+else:
+    print("This is not a Leap year")
