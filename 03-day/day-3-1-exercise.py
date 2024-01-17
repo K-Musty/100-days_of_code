101ed6cf08f0e8e93d601e27f8c215badd7ffeed
K-Musty <kmustapha9564@gmail.com>
2024-01-17 06:32:30 -0800

    This commit woul;d push day 3 task 1

--- 03-day/day-3-1-exercise.py
+++ 03-day/day-3-1-exercise.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+#checking wheather number is even or odd
+
+number = int(input("Please input number"))
+
+if number % 2 == 0:
+    print("This number is an even number")
+else:
+    print("This number is an odd number")
 
