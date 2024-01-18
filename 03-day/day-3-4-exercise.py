78f6bc6e1811f3ebcc1a3c622eda1f7f6e227249
K-Musty <kmustapha9564@gmail.com>
2024-01-18 14:52:03 -0800

    day 3 task 4

--- 03-day/day-3-4-exercise.py
+++ 03-day/day-3-4-exercise.py
@@ -4,36 +4,31 @@ size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 
-price_for_small = 15
-price_for_medium = 20
-price_for_large = 25
-price_for_pepperoni_s = 2
-price_for_pepperoni_m_l = 3
-price_for_cheese = 1
+price = 0
 
-if size == S or size == s:
-    size = price_for_small
-    if add_pepperoni == y:
-        if extre_cheese == y:
-            print
-        else
+
+if size == 'S' or size == 's':
+    price += 15
+elif size == 'M' or size == 'm':
+    price += 20
+elif size == 'L' or size == 'l':
+    price += 25
+else:
+    print("input Error: 98!")
+
+if add_pepperoni == 'Y' or add_pepperoni == 'y':
+    if size == 'S' or size == 's':
+        price += 2
+    elif size == 'm' or size == 'M':
+        price += 3
+    elif size == 'L' or size == 'l':
+        price += 3
     else:
+        price += 0
+
+if extra_cheese == 'Y' or extra_cheese == "y":
+    price += 1
 else:
-    print(f"your final bill is ${price_for_small}")
-elif size == M or size == m:
-    if add_pepperoni == y:
-        if extra_cheese == y:
-            print
-        else
-    else
-else:
-    print(f"Your final bill is ${price_for_medium}")
-elif size == L or size == l:
-    size = price_for_large
-    if add_pepperoni == y:
-        if extra_cheese == y:
-            print
-        else
-    else
-else:
-    print(f"Your final bill is ${price_for_large}")
+    price += 0
+
+print(f"You total bill is ${price}")
