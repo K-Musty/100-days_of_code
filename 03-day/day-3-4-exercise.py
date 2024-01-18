8af85313dcb8c5307b48a0ff41d5b0aea76457b0
K-Musty <kmustapha9564@gmail.com>
2024-01-18 14:07:19 -0800

    day 3 task 4

---
+++ 03-day/day-3-4-exercise.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+price_for_small = 15
+price_for_medium = 20
+price_for_large = 25
+price_for_pepperoni_s = 2
+price_for_pepperoni_m_l = 3
+price_for_cheese = 1
+
+if size == S or size == s:
+    size = price_for_small
+    if add_pepperoni == y:
+        if extre_cheese == y:
+            print
+        else
+    else:
+else:
+    print(f"your final bill is ${price_for_small}")
+elif size == M or size == m:
+    if add_pepperoni == y:
+        if extra_cheese == y:
+            print
+        else
+    else
+else:
+    print(f"Your final bill is ${price_for_medium}")
+elif size == L or size == l:
+    size = price_for_large
+    if add_pepperoni == y:
+        if extra_cheese == y:
+            print
+        else
+    else
+else:
+    print(f"Your final bill is ${price_for_large}")
