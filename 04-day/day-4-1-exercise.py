8b93ba54e29a35b7a72e44db67aad39cc7e2a156
K-Musty <kmustapha9564@gmail.com>
2024-01-24 06:07:04 -0800

    day 4 task 1

---
+++ 04-day/day-4-1-exercise.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+coin_face = random.radint(0,1)
+if coin_face == 1:
+    print("Heads")
+else:
+    print("Tails")
+
