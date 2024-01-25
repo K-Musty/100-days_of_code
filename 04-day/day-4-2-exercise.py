f70c4c6b3c1c851475501fac2c567c3e8ee4b1fb
K-Musty <kmustapha9564@gmail.com>
2024-01-25 13:52:19 -0800

    day 4 task 2 complete

--- 04-day/day-4-2-exercise.py
+++ 04-day/day-4-2-exercise.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
+import random
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+namesAsCSV = input("Give me everybody's names, separated by comma. ")
+
+names = namesAsCSV.split(", ")
+
+#you can use random.choice
+maximumnumber = len(names)
+
+randomPerson = random.randint(0, maximumnumber - 1)
+
+person_to_pay = names[randomPerson]
+print(person_to_pay + " Go buy")
+
+
 
