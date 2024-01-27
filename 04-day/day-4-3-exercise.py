c49171cdf3e93afb0f5cc3d8d9bb1531f57638ed
K-Musty <kmustapha9564@gmail.com>
2024-01-27 12:04:20 -0800

    task 3 day 4 completed

--- 04-day/day-4-3-exercise.py
+++ 04-day/day-4-3-exercise.py
@@ -7,10 +7,10 @@ map = [row1, row2, row3]
 print(f"{row1}\n{row2}\n{row3}")
 position = input("Where do you want to put the treasure? ")
 
-y-axis = int(position[0])
-x-axis = int(position[1])
+axis_y = int(position[0])
+axis_x = int(position[1])
 
-selected_row = map[x-axis - 1]
-selected_row[y-axis - 1] = "X"
+selected_row = map[axis_x - 1]
+selected_row[axis_y - 1] = "X"
 
 print(f"{row1}\n{row2}\n{row3}")
