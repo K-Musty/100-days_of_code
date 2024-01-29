10853df7953fe77d0ffb812d38007fa6a50c3a10
K-Musty <kmustapha9564@gmail.com>
2024-01-29 15:00:22 -0800

    rps game

--- 04-day/rock-paper-scissors.py
+++ 04-day/rock-paper-scissors.py
@@ -1,2 +1,39 @@
 #!/usr/bin/python3
 print("Rock, Paper and Scissors Game!!!")
+
+rock = """
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+
+paper = """
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+
+scissors = """
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+rps_choice = [rock, paper, scissors]
+
+player_input = input("Choose 0 for rock, 1 for paper and 2 for scissors ")
+
+player_choice = int(player_input)
+
+if player_choice == 0:
+    print(rock)
+    if
