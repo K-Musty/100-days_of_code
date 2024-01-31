0bb19db2c36fa5252845dda265e8f3c366901441
K-Musty <kmustapha9564@gmail.com>
2024-01-31 12:56:37 -0800

    day 4 completed

--- 04-day/rock-paper-scissors.py
+++ 04-day/rock-paper-scissors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
 import random
+
 print("Rock, Paper and Scissors Game!!!")
 
 rock = """
@@ -30,13 +30,25 @@ scissors = """
 ---.__(___)
 """
 
-rps_choice = [rock, paper, scissors]
 
 player_input = input("Choose 0 for rock, 1 for paper and 2 for scissors ")
-
+display_choice = int(player_input)
+if display_choice == 0:
+    print(rock)
+elif display_choice == 1:
+    print(paper)
+elif display_choice == 2:
+    print(scissors)
 player_choice = int(player_input)
 
 computer_choice = random.randint(0,2)
+computer_display = computer_choice
+if computer_display == 0:
+    print(rock)
+elif computer_display == 1:
+    print(paper)
+elif computer_display == 2:
+    print(scissors)
 
 if player_choice == 0 and computer_choice == 0:
     print("This is a draw, Play Again!!")
