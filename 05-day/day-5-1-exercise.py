7a6309bb2dec2e0e92fe10b96af8087770f6db5a
K-Musty <kmustapha9564@gmail.com>
2024-02-03 12:46:06 -0800

    day 5 task 1

--- 05-day/day-5-1-exercise.py
+++ 05-day/day-5-1-exercise.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+student_heights = input("input a list of student height ").split()
+for n in range(0, len(student_heights[n])):
+   student_heights[n] = int(student_heights[n])
+print(student_heights)
+
+height_sum = 0
+for height_sum in student_heights:
+    height_sum += student_heights
+print(height_sum)
