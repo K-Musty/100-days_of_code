fe348713674585f3c8203728c15f562b0d3d6469
K-Musty <kmustapha9564@gmail.com>
2024-02-03 13:10:39 -0800

    day 5 task 1 complete

--- 05-day/day-5-1-exercise.py
+++ 05-day/day-5-1-exercise.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 student_heights = input("input a list of student height ").split()
-for n in range(0, len(student_heights[n])):
+for n in range(0, len(student_heights)):
    student_heights[n] = int(student_heights[n])
 print(student_heights)
 
 height_sum = 0
-for height_sum in student_heights:
-    height_sum += student_heights
-print(height_sum)
+for height in student_heights:
+    height_sum += height
+#sum of height
+
+student_number = 0
+for number in student_heights:
+    student_number += 1
+#number of items
+
+average_height = round(height_sum / student_number)
+print(average_height)
