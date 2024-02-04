518074fe422657a6d334163d3e6796f4e9c85ea6
K-Musty <kmustapha9564@gmail.com>
2024-02-04 01:28:17 -0800

    day 5 task 2

---
+++ 05-day/day-5-2-exercise.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+student_scores = input("Input a list of student score ")
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+highest_score = 0
+for score in student_scores:
+    if score > hightest_score
+    score = highest_score
+print(highest_score)
