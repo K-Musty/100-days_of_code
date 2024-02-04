ee302a73b3b457de99d1b524b213098665fe2177
K-Musty <kmustapha9564@gmail.com>
2024-02-04 01:47:13 -0800

    day 5 task 2 complete

--- 05-day/day-5-2-exercise.py
+++ 05-day/day-5-2-exercise.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 
-student_scores = input("Input a list of student score ")
+student_scores = input("Input a list of student score ").split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 print(student_scores)
 
 highest_score = 0
 for score in student_scores:
-    if score > hightest_score
-    score = highest_score
-print(highest_score)
+    if score > highest_score:
+       highest_score = score
+print(f"The highest core in the class is: {highest_score}")
+
+lowest_score = student_scores[0]
+for score in student_scores:
+    if score < lowest_score:
+       lowest_score = score
+print(f"The lowest score in the class is: {lowest_score}")
