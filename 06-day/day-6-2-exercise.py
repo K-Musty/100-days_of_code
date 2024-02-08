a3d94ac65c8ae4d6b67ff9e74fb4af2656f44b0e
K-Musty <kmustapha9564@gmail.com>
2024-02-08 22:06:42 +0100

    day 6 REEborgs world challenge 2

---
+++ 06-day/day-6-2-exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+''' Second jump game '''
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
