e49d8e1ade032f1afbdcbb8947b79bc7e33f0e33
K-Musty <kmustapha9564@gmail.com>
2024-02-11 16:56:56 +0100

    Updates on README to describe more about the project

--- 07-day/Day-7-Hangman-1-Start.py
+++ 07-day/Day-7-Hangman-1-Start.py
@@ -3,7 +3,7 @@ import random
 ''' Hangman Game First Challenge '''
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-input_guess = input("Guess a letter")
+input_guess = input("Guess a letter: ")
 guess = input_guess.lower()
 
 for char in chosen_word:
--- 07-day/README.md
+++ 07-day/README.md
@@ -1,1 +1,6 @@
 # DAY 7 OF 100
+1. Picking and Checking Random words
+2. Replacing blank with guesses
+3. Checking if player has won
+4. Keeping Track of lives
+5. Improving User experience
