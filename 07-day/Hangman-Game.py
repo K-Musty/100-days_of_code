0cfe6e56d663d1aae9c9a7c381fc42bd802d1c58
K-Musty <kmustapha9564@gmail.com>
2024-02-13 22:21:39 +0100

    Day 7 complete: Hangman Game

--- 07-day/Hangman-Game.py
+++ 07-day/Hangman-Game.py
@@ -8,9 +8,8 @@ import random
 from stages import stages
 from Hangmanwords_logo import logo, word_list
 print(logo)
+
 chosen_word = random.choice(word_list)
-#Testing code
-print(f'psst, the soluton is {chosen_word}.')
 word_len = len(chosen_word)
 lives = 6
 
@@ -19,17 +18,20 @@ for char in range(word_len):
     display += '_'
 print(display)
 
+
 the_end = False
 while not the_end:
     input_guess = input("Guess a letter: ")
     guess = input_guess.lower()
-
+    if guess in display:
+        print(f"You've guessed letter {guess}, Try again") 
     for position in range(word_len):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
 
     if guess not in chosen_word:
+        print(f"The letter {guess} is not correct, You lose a life")
         lives -= 1
         if lives == 0:
             the_end = True
