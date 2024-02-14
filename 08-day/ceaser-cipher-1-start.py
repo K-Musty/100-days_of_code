5fd0cc25cb307c8daac1071c21e2ca0220742e33
K-Musty <kmustapha9564@gmail.com>
2024-02-14 23:45:06 +0100

    Ceaser Cipher task 1 complete

--- 08-day/ceaser-cipher-1-start.py
+++ 08-day/ceaser-cipher-1-start.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+text = input("Type your message: \n").lower()
+shift = int(input("Type the shift number: "))
 
 #Don't change the code above 👆
-def encrpyt (text_field, shift_field):
+def encrypt (text_field, shift_field):
+    cipher_text = ""
     for letter in text_field:
-        position = aphabet.indes(letter)
+        position = alphabet.index(letter)
         new_position = position + shift_field
         new_letter = alphabet[new_position]
-        print(new_letter)
+        cipher_text += new_letter
+    print(cipher_text)
 
-encrypt(textfield=text, shift_field=shift)
+encrypt(text_field=text, shift_field=shift)
 
