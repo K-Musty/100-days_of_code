a879a0ed48cf3e2b7c6f44dcc6038b40ddc2ffc1
K-Musty <kmustapha9564@gmail.com>
2024-02-15 21:09:44 +0100

    'ceaser-cipher task 2

---
+++ 08-day/ceaser-cipher-2-start.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message: \n").lower()
+shift = int(input("Type the shift number: "))
+
+#Don't change the code above 👆
+def encrypt (text_field, shift_field):
+    cipher_text = ""
+    for letter in text_field:
+        position = alphabet.index(letter)
+        new_position = position + shift_field
+        new_letter = alphabet[new_position]
+        cipher_text += new_letter
+    print(cipher_text)
+
+encrypt(text_field=text, shift_field=shift)
+
+def decrypt(text_field, shift_field):
+
