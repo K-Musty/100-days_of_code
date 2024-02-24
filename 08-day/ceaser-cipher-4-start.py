6bc5c5de63c4cef8fd1e426dbba3573158a7ac85
K-Musty <kmustapha9564@gmail.com>
2024-02-23 16:47:54 -0800

    Day 8 Ceaser cipher task 3

--- 08-day/ceaser-cipher-4-start.py
+++ 08-day/ceaser-cipher-4-start.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-from art import logo
-print(logo)
-
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
@@ -9,17 +6,22 @@ text = input("Type your message: \n").lower()
 shift = int(input("Type the shift number: "))
 
 #Don't change the code above 👆
-def ceaser(text_field, shift_field, cipher_direction):
-    end_text = ""
-    for letter in text_field:
-        position = alphabet.index(letter)
-        if cipher_direction == "encode":
+def ceaser(text_field, shift_field):
+    cipher_text = ""
+    decipher_text = ""
+    if direction == "encode":
+        for letter in text_field:
+            position = alphabet.index(letter)
             new_position = position + shift_field
             new_letter = alphabet[new_position]
-            end_text += new_letter
-        elif cipher_direction == "decode":
+            cipher_text += new_letter
+        print(cipher_text)
+    elif direction == "decode":
+        for letter in text_field:
+            position = alphabet.index(letter)
             new_position = position - shift_field
-            new_letter = alphabet[new_position]
-            end_text += new_letter
-    print(f"The {cipher_direction}d text is {end_text}")
-ceaser(text, shift, direction)
+            decipher_text += alphabet[new_position]
+        print(decipher_text)
+from art import logo
+print(logo)
+
