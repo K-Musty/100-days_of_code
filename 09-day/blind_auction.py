c1309aa665b0d33aaedb0beda655df96f35e4660
K-Musty <kmustapha9564@gmail.com>
2024-03-04 20:59:06 +0100

    Blind Auction: 1. i need to import replit clear function 2. i need to go get the logo

--- 09-day/blind_auction.py
+++ 09-day/blind_auction.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python3
-from art import logo
+#from art import logo
 
-print(logo)
+#print(logo)
 print("Welcome to the blind Auction")
 
 auction_dict = {}
 
-continue_bid = false
-while not true:
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+continue_bid = False
+while not continue_bid:
     name = input("What is your name?: ")
     bid_price = int(input("How much would you want to bid?: $"))
     auction_dict[name] = bid_price
     restart = input("Do you want to cotinue, 'yes' or 'no' ")
-    if restart = 'no':
-        continue_bid = true
-    
+    if restart == 'no':
+        continue_bid = True
 
 
+find_highest_bidder(auction_dict)
