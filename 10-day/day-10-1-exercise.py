eecf0af6ef51f524d0fccf61160a000e2f71b385
K-Musty <kmustapha9564@gmail.com>
2024-03-31 23:58:10 +0100

    Day 10 exercise 1

--- 10-day/day-10-1-exercise.py
+++ 10-day/day-10-1-exercise.py
@@ -1,1 +1,9 @@
-#!/bin/python3
+#!/usr/bin/python3
+#Function with Outputs
+
+def format_name(f_name, l_name):
+    first = f_name.title()
+    last = l_name.title()
+    return f"{first} {last}"
+formated_string = format_name("AbbaA", "ABBAA")
+print(formated_string)
