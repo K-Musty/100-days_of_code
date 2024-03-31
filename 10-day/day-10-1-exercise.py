32e6fa0aa12bca71c532e948a1fd63ac117d570a
K-Musty <kmustapha9564@gmail.com>
2024-04-01 00:17:11 +0100

    More C hanges:

--- 10-day/day-10-1-exercise.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-#Function with Outputs
-
-def format_name(f_name, l_name):
-    first = f_name.title()
-    last = l_name.title()
-    return f"{first} {last}"
-formated_string = format_name("AbbaA", "ABBAA")
-print(formated_string)
