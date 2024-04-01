fb859b4eb5972ce9c00525b7a8e9a92616779035
K-Musty <kmustapha9564@gmail.com>
2024-04-01 23:39:34 +0100

    Day 10 calculator first task complete

--- 10-day/calculator-start-1.py
+++ 10-day/calculator-start-1.py
@@ -28,4 +28,20 @@ for keys in operators:
 
 operation_symbol = input("Pick an operation from the line above ")
 
+calculations = operators[operation_symbol]
+answer = calculation_function(num1, num2)
 
+print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+'''
+if operation_symbol == "+":
+    add(num1, num2)
+elif operational_symbol == "-":
+    substract(num1, num2)
+elif operational_symbol == "*":
+    multiply(num1, num2)
+elif operational_symbol == "/":
+    divide(num1, num2)
+else:
+    print("Error: invalid operator!!!")
+'''
