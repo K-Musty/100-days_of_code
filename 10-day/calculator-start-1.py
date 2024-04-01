72b7a9f3a52d9a27acb02e14b5a06b343829476d
K-Musty <kmustapha9564@gmail.com>
2024-04-01 23:19:30 +0100

    Creates functions

--- 10-day/calculator-start-1.py
+++ 10-day/calculator-start-1.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 #Calculator project challenge one
+
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def div(n1, n2):
+    return n1 / n2
+
