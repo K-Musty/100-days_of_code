ed19a376a54bfa2caf3c6dcdd5f4a2b2c12b1ccd
K-Musty <kmustapha9564@gmail.com>
2024-04-01 01:09:07 +0100

    Day 10 Task: Leap year and month task updated and completed

--- 10-day/day-10-1-exercise.py
+++ 10-day/day-10-1-exercise.py
@@ -2,23 +2,29 @@
 #Leap year calculator
 
 print("Leap year checker!")
-year = int(input("Which year do you want to check? "))
 
 
-if year % 4 == 0:
-    if year % 100 != 0:
-        return True
-    elif year % 400 == 0:
-        return False
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 != 0:
+            return True
+        elif year % 400 == 0:
+            return False
+        else:
+            return True
     else:
-        return True
-else:
-    return False
+        return False
 
 def days_in_month(year, month):
+    if month > 12:
+        return "too Much"
+    if month < 1:
+        return "Too small"
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month -1]
 year = int(input("Enter a year: "))
-month = int(input("Enter a month"))
+month = int(input("Enter a month "))
 days = days_in_month(year, month)
 print(days)
