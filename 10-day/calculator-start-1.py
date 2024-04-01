ff90bb561d42f176f44c84d1830ef46f73cad100
K-Musty <kmustapha9564@gmail.com>
2024-04-01 23:29:03 +0100

    For loop through dictionaries

--- 10-day/calculator-start-1.py
+++ 10-day/calculator-start-1.py
@@ -4,12 +4,28 @@
 def add(n1, n2):
     return n1 + n2
 
-def sub(n1, n2):
+def substract(n1, n2):
     return n1 - n2
 
-def mul(n1, n2):
+def multiply(n1, n2):
     return n1 * n2
 
-def div(n1, n2):
+def divide(n1, n2):
     return n1 / n2
+
+operators = {
+        "+": add,
+        "-": substract,
+        "*": multiply,
+        "/": divide
+}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for keys in operators:
+    print(keys)
+
+operation_symbol = input("Pick an operation from the line above ")
+
 
