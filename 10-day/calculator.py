809669aa81dc5644b7b671564b4cffb15cf09b26
K-Musty <kmustapha9564@gmail.com>
2024-04-04 00:05:16 +0100

    calculator: completed day 10

--- 10-day/calculator.py
+++ 10-day/calculator.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 #Calculator project challenge one
 from art import logo
-print(logo)
 def add(n1, n2):
     return n1 + n2
 
@@ -21,14 +20,15 @@ operators = {
         "/": divide
 }
 def calculator():
-    num1 = int(input("What's the first number?: "))
+    print(logo)
+    num1 = float(input("What's the first number?: "))
     for keys in operators:
         print(keys)
     should_continue = True
     while should_continue:
         operation_symbol = input("Pick an operation ")
 
-        num2 = int(input("What's the next number?: "))
+        num2 = float(input("What's the next number?: "))
         calculations = operators[operation_symbol]
         answer = calculations(num1, num2)
 
@@ -44,15 +44,3 @@ def calculator():
 
 calculator()
 
-'''
-if operation_symbol == "+":
-    add(num1, num2)
-elif operational_symbol == "-":
-    substract(num1, num2)
-elif operational_symbol == "*":
-    multiply(num1, num2)
-elif operational_symbol == "/":
-    divide(num1, num2)
-else:
-    print("Error: invalid operator!!!")
-'''
