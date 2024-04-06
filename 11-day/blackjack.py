9b30db454d92914a3d18fac14b950d9b2636324c
K-Musty <kmustapha9564@gmail.com>
2024-04-06 23:03:49 +0100

    Blackjack: completed for now although minor changes still needed

--- 11-day/blackjack.py
+++ 11-day/blackjack.py
@@ -5,9 +5,6 @@ def get_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9,10, 10, 10, 10]
     card_taken = random.choice(cards)
     return card_taken
-user_card = []
-computer_card = []
-game_over = false
 
 def compare(user_score, computer_score):
     if user_score == computer_score:
@@ -25,6 +22,10 @@ def compare(user_score, computer_score):
     else:
         return "You lose"
 def begin_game():
+    user_card = []
+    computer_card = []
+    game_over = False
+
     for n in range(0,2):
         user_card.append(get_card())
         computer_card.append(get_card())
@@ -33,7 +34,7 @@ def begin_game():
         total_sum = sum(cards)
         return total_sum
     
-    whlie not game_over:
+    while not game_over:
         user_sum = calculate_sum(user_card)
         computer_sum = calculate_sum(computer_card)
 
@@ -50,7 +51,7 @@ def begin_game():
         computer_score = calculate_score(computer_card)
 
         print(f"Youre cards are {user_card} and Score is {user_score} ")
-        print(f"Computer cards are {computer_card} and Score is {computer_score} ")
+        print(f"Computer cards are {computer_card[0]} and Score is {computer_score} ")
 
         if computer_score == 0 or user_score == 0 or user_score > 21:
             game_over = True
@@ -60,9 +61,9 @@ def begin_game():
                 user_card.append(get_card())
             else:
                 game_over = True
-    while computer_score != 0 and computer_score < 17:
-        computer_card.append(get_card())
-        computer_score = calculate_score(computer card)
+        while computer_score != 0 and computer_score < 17:
+            computer_card.append(get_card())
+            computer_score = calculate_score(computer_card)
 
     print(compare(user_score, computer_score))
 
