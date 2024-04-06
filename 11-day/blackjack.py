907613574eee1c1ca3b35379ebd3b8f4c9ec9b1f
K-Musty <kmustapha9564@gmail.com>
2024-04-06 22:33:07 +0100

    Blackjack: calcuklating logic

--- 11-day/blackjack.py
+++ 11-day/blackjack.py
@@ -7,42 +7,50 @@ def get_card():
     return card_taken
 user_card = []
 computer_card = []
+game_over = false
 
 
 def begin_game():
     for n in range(0,2):
         user_card.append(get_card())
         computer_card.append(get_card())
-    print(user_card)
-    print(computer_card[0])
 
     def calculate_sum(cards):
         total_sum = sum(cards)
         return total_sum
-
-    user_sum = calculate_sum(user_card)
-    computer_sum = calculate_sum(computer_card)
     
-    def calculate_score(cards):
-        if sum(cards) == 21 and len(cards) == 2:
-            return 0
-        if 11 in cards and sum(cards) > 21:
-            cards.remove(11)
-            cards.append(1)
+    whlie not game_over:
+        user_sum = calculate_sum(user_card)
+        computer_sum = calculate_sum(computer_card)
 
-    
-    if computer_sum == 21 and user_sum != 21:
-        print("computer wins")
-    elif computer_sum != 21 and user_sum == 21:
-        print("You win Hooray")
-    elif computer_sum == 21 and user_sum == 21:
-        print("This game is a Draw, please try again")
-    elif computer_sum > 21 and user_sum > 21:
-        print("This game is a Draw, please try again")
-    elif computer_sum > 21 and user_sum < 21:
-        print("You win Hooray")
-    elif computer_sum < 21 and user_sum > 21:
-        print("You lose, computer wins")
+        def calculate_score(cards):
+            total_sum = sum(cards)
+            return total_sum
+            if sum(cards) == 21 and len(cards) == 2:
+                return 0
+            if 11 in cards and sum(cards) > 21:
+                cards.remove(11)
+                cards.append(1)
+
+        user_score = calculate_score(user_card)
+        computer_score = calculate_score(computer_card)
+
+        print(f"Youre cards are {user_card} and Score is {user_score} ")
+        print(f"Computer cards are {computer_card} and Score is {computer_score} ")
+
+        if computer_score == 0 or user_score == 0 or user_score > 21:
+            game_over = True
+        else:
+            pick_another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+            if pick_another_card == "y":
+                user_card.append(get_card())
+            else:
+                game_over = True
+    while computer_score != 0 and computer_score < 17:
+        computer_card.append(get_card())
+        computer_score = calculate_score(computer card)
+
+
     
 
 begin_game()
