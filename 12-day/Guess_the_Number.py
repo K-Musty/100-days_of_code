7d2c860e02985698567465bef415d14769549817
K-Musty <kmustapha9564@gmail.com>
2024-04-13 23:44:51 +0100

    Added a lives and try functions

--- 12-day/Guess_the_Number.py
+++ 12-day/Guess_the_Number.py
@@ -8,49 +8,27 @@ print(logo)
 
 answer = randint(1, 100)
 
-def easy_mode():
-    game_over = False
-    tries = 10
-    while not game_over:
-        for n in tries:
-            n -= tries
-        game_over = True
-def hard_mode():
-    game_over = False
-    tries = 4
-    while not game_over:
-        for n in tries:
-            n -= tries
-        game_over = True
-        
-def level_game():
-    level = int(input("Select '1' for easy and '2' for hard "))
-    if level == 1:
-        easy_mode()
-        compare(guess, answer)
-    elif level == 2:
-        hard_mode()
-        compare(guess, answer)
-
-    else:
-        print("Wrong choice: Try again !!")
-
 def begin_game():
+    max_tries = 5
+    tries = 0
     game_over = False
-    while not game_over:
+    while tries < max_tries:
         guess = int(input("choose a number from 1 to 100: "))
         def compare(guess, answer):
+            nonlocal tries
             if guess > answer:
                 print("Too High")
             elif guess < answer:
                 print("Too Low")
             elif guess == answer:
                 print("Guess is Correct!!!")
-                game_over = True
+                return True
             else:
-                print("invalid input!!!")
-                game_over = True
+                print("invalid input!!")
+            tries += 1
+            return false
         compare(guess, answer)
         if guess == answer:
-            game_over = True
+            return True
+
 begin_game()
