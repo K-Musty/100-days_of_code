ddff7da8551fa75188b1ae36e818b60c888f457a
K-Musty <kmustapha9564@gmail.com>
2024-04-14 23:30:57 +0100

    Guess the number game

--- 12-day/Guess_the_Number.py
+++ 12-day/Guess_the_Number.py
@@ -8,14 +8,13 @@ print(logo)
 
 answer = randint(1, 100)
 
-def begin_game():
-    max_tries = 5
-    tries = 0
-    game_over = False
-    while tries < max_tries:
+def hard_game():
+    end_tries = 0
+    tries = 5
+    while tries > end_tries:
+        print(f"You have {tries} tries left")
         guess = int(input("choose a number from 1 to 100: "))
         def compare(guess, answer):
-            nonlocal tries
             if guess > answer:
                 print("Too High")
             elif guess < answer:
@@ -25,10 +24,42 @@ def begin_game():
                 return True
             else:
                 print("invalid input!!")
-            tries += 1
-            return false
+        tries -= 1
+        return False 
         compare(guess, answer)
         if guess == answer:
+            print("You loose, try again")
             return True
+
+def easy_game():
+    end_tries = 0
+    tries = 10
+    while tries > end_tries:
+        print(f"You have {tries} tries left")
+        guess = int(input("choose a number from 1 to 100: "))
+        def compare(guess, answer):
+            if guess > answer:
+                print("Too High")
+            elif guess < answer:
+                print("Too Low")
+            elif guess == answer:
+                print("Guess is Correct!!!")
+                return True
+            else:
+                print("invalid input!!")
+        tries -= 1
+        return False
+        compare(guess, answer)
+        if guess == answer:
+            print("You loose")
+            return True
+
+def begin_game():
+    level = int(input("Choose '1' for easy and '2' for hard: "))
+    if level == 1:
+        print("yes")
+        easy_game()
+    elif level == 2:
+        hard_game()
 
 begin_game()
