1ae0dca7ffc0201ab32e1ae9009f2b0ae4822658
K-Musty <kmustapha9564@gmail.com>
2024-05-03 23:20:01 +0100

    Day 13- 2nd challenge

--- 13-day/day-13-exercise.py
+++ 13-day/day-13-exercise.py
@@ -1,17 +1,19 @@
 ############DEBUGGING#####################
+#!/usr/bin/python3
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+'''def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
 
-# # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+my_function()
+'''
+
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 7)
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
