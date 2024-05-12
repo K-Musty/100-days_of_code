6f7f48b19f2a2f5011de8555e4d61b83e5058fe2
K-Musty <kmustapha9564@gmail.com>
2024-05-12 23:19:55 +0100

    commit

---
+++ 13-day/age.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+#Legal age
+age = int(input("How old are you?"))
+if age < 18:
+    print(f"You are not eligable because {age} isn't old enough")
--- 13-day/day-13-exercise.py
+++ 13-day/day-13-exercise.py
@@ -1,19 +1,20 @@
 ############DEBUGGING#####################
 #!/usr/bin/python3
 
-'''def my_function():
-    for i in range(1, 21):
-        if i == 20:
-            print("You got it")
+#'''def my_function():
+#    for i in range(1, 21):
+#
+#if i == 20:
+#            print("You got it")
 
-my_function()
-'''
+#my_function()
+#'''
 
 # Reproduce the Bug
-from random import randint
-dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(1, 7)
-print(dice_imgs[dice_num])
+#from random import randint
+#dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+#dice_num = randint(1, 7)
+#print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
@@ -22,10 +23,10 @@ print(dice_imgs[dice_num])
 # elif year > 1994:
 #   print("You are a Gen Z.")
 
-# # Fix the Errors
-# age = input("How old are you?")
-# if age > 18:
-# print("You can drive at age {age}.")
+# Fix the Errors
+age = int(input("How old are you?"))
+if age > 18:
+    print(f"You can drive at age {age}.")
 
 # #Print is Your Friend
 # pages = 0
