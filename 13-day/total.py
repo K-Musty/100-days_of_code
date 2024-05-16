484492985c77cda75b02ffe21c9a9ada1f9e46c8
K-Musty <kmustapha9564@gmail.com>
2024-05-16 21:25:45 +0100

    Total number of words debugging challenge

---
+++ 13-day/total.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#Print is Your Friend
+pages = int(input("Enter number of pages: "))
+word_per_page = int(input("Enter words per pages: "))
+
+total_words = pages * word_per_page
+print(f"The total number of words you have is {total_words} in total")
