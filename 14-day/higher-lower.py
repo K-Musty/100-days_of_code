14faba44b653c05110456c6ba1632d75c87d71be
K-Musty <kmustapha9564@gmail.com>
2024-06-04 17:51:15 +0100

    Day 14: Higher lower game

--- 14-day/higher-lower.py
+++ 14-day/higher-lower.py
@@ -14,6 +14,15 @@ def format_data(entity):
     entity_country = entity["country"]
     return f"{entity_name}, a {entity_descrpt}, from {entity_country}"
 
+def check_answer(guess, a_followers, b_followers):
+    """Take the user guess and follower counts and returns if they are right"""
+    if a_followers > b_followers:
+        return guess == "a"
+    elif b_followers > a_followers:
+        return guess == "b"
+    else:
+        return false
+
 
 entity_a = random.choice(data)
 entity_b = random.choice(data)
@@ -28,3 +37,16 @@ print(f"Compare A: {compare_a}")
 print(vs)
 print(f"Against B: {compare_b}")
 
+guess = input("Who has more followers, A or B: ").lower()
+
+a_follower_account = entity_a["follower_count"]
+b_follower_account = entity_b["follower_count"]
+
+is_correct = check_answer(guess, a_follower_account, b_follower_account)
+
+score = 0
+if is_correct:
+    score += 1
+    print(f"You're right! Current score: {score}.")
+else:
+    print("Sorry, that's wrong. Final score: {score}.")
