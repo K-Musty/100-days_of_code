a01bb127c58c638603c7bfaea2d74c83cd2e4cd8
K-Musty <kmustapha9564@gmail.com>
2024-07-16 23:12:18 +0100

    turtle Screen attribute to object my_screen

--- 16-day/day-16-start/main.py
+++ 16-day/day-16-start/main.py
@@ -2,7 +2,11 @@
 # import another_module
 # print(another_module.another_variable);
 
-from turtle import Turtle
+from turtle import Turtle, Screen
 # Turtle class
 timmy = Turtle()
 print(timmy)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
