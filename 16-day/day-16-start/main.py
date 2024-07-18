4b669d464d62fca99bd975a2eadebca3e4b74636
K-Musty <kmustapha9564@gmail.com>
2024-07-18 21:24:54 +0100

    Day 16: update

--- 16-day/day-16-start/main.py
+++ 16-day/day-16-start/main.py
@@ -3,19 +3,21 @@
 # print(another_module.another_variable);
 
 from turtle import Turtle, Screen
+
 # Turtle class
 timmy = Turtle()
 print(timmy)
 timmy.shape("turtle")
+
 #changing color challenge
 timmy.color("aquamarine3", "DarkOliveGreen4")
+
 #maving 100 paces challenge
 timmy.forward(100)
 timmy.left(120)
 timmy.forward(100)
 timmy.left(120)
 timmy.forward(100)
-
 
 #Screen display
 my_screen = Screen()
