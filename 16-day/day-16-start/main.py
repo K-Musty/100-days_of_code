e08a5a12872a339da118bab4e32c1af3f5937f6c
K-Musty <kmustapha9564@gmail.com>
2024-07-18 21:43:01 +0100

    PrettyTable installed

--- 16-day/day-16-start/main.py
+++ 16-day/day-16-start/main.py
@@ -1,26 +1,28 @@
-#!/usr/bin/python3
-# import another_module
-# print(another_module.another_variable);
+# #!/usr/bin/python3
+# # import another_module
+# # print(another_module.another_variable);
+#
+# from turtle import Turtle, Screen
+#
+# # Turtle class
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+#
+# #changing color challenge
+# timmy.color("aquamarine3", "DarkOliveGreen4")
+#
+# #maving 100 paces challenge
+# timmy.forward(100)
+# timmy.left(120)
+# timmy.forward(100)
+# timmy.left(120)
+# timmy.forward(100)
+#
+# #Screen display
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-from turtle import Turtle, Screen
-
-# Turtle class
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-
-#changing color challenge
-timmy.color("aquamarine3", "DarkOliveGreen4")
-
-#maving 100 paces challenge
-timmy.forward(100)
-timmy.left(120)
-timmy.forward(100)
-timmy.left(120)
-timmy.forward(100)
-
-#Screen display
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
-
+#Pretty table packege import
+import prettytable
