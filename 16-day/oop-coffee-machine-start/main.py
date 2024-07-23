04ba6edf0b2aacfc25e2fed0502423d707268b45
K-Musty <kmustapha9564@gmail.com>
2024-07-23 23:58:19 +0100

    Day 16: OOP

--- 16-day/oop-coffee-machine-start/main.py
+++ 16-day/oop-coffee-machine-start/main.py
@@ -3,3 +3,14 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine#!/usr/bin/pyhton3
 
+ingredients = MenuItem
+menu = Menu()
+process_money = MoneyMachine()
+process_coffee = CoffeeMacker()
+machine_start = True
+print(Welcome to Coffee Machine)
+
+while machine_start:
+    userInput = input("What would you like? (espresso/latte/cappuccino)? ")
+    if userInput == "off":
+        machine_start = False
