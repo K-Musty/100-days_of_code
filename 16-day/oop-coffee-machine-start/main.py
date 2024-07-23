d0fe3b1ef9537048590c4be5ecc9b2815eb491b4
K-Musty <kmustapha9564@gmail.com>
2024-07-24 00:07:05 +0100

    DAY 16: OOP

--- 16-day/oop-coffee-machine-start/main.py
+++ 16-day/oop-coffee-machine-start/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine#!/usr/bin/pyhton3
@@ -14,3 +14,11 @@ while machine_start:
     userInput = input("What would you like? (espresso/latte/cappuccino)? ")
     if userInput == "off":
         machine_start = False
+    elif userInput == "report":
+        process_coffee.report()
+        process_money.report()
+    else:
+        drink = menu.find_drink(userInput)
+        if process_coffee.is_resource_sufficient(drink):
+            if process_money.make_payment(drink.cost):
+                process_coffee.make_coffee(drink)
