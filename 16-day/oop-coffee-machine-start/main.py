8baf919011949ea83d727354e1b508b46f0f103e
K-Musty <kmustapha9564@gmail.com>
2024-07-25 23:35:51 +0100

    merged

--- 16-day/oop-coffee-machine-start/main.py
+++ 16-day/oop-coffee-machine-start/main.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
-from money_machine import MoneyMachine#!/usr/bin/pyhton3
+from money_machine import MoneyMachine
 
 ingredients = MenuItem
 menu = Menu()
 process_money = MoneyMachine()
-process_coffee = CoffeeMacker()
+process_coffee = CoffeeMaker()
 machine_start = True
-print(Welcome to Coffee Machine)
+print("Welcome to Coffee Machine")
 
 while machine_start:
     userInput = input("What would you like? (espresso/latte/cappuccino)? ")
