046070229294a003760bc5b0f2cc40256d25b8f4
K-Musty <kmustapha9564@gmail.com>
2024-08-17 18:24:55 +0100

    Day 17

--- 17-day/quiz_brain.py
+++ 17-day/quiz_brain.py
@@ -1,25 +1,30 @@
+#!/usr/bin/env pyhton3
 class QuizBrain:
-  def __init__(self, q_list):
-    self.question_number = 0
-    self.question_list = q_list
-    self.score = 0
-    
-  def still_has_questions(self):
-    return self.question_number < len(self.question_list)      
-  
-  def next_question(self):
-    current_question = self.question_list[self.question_number]
-    self.question_number += 1
-    user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")  
-    self.check_answer(user_answer, current_question.answer)   
 
-  def check_answer(self, user_answer, correct_answer): 
-    if user_answer.lower() == correct_answer.lower():
-        self.score += 1
-        print("You got it right!")
-    else:
-        print("That's not quite right.")
-    print(f"The correct answer was {correct_answer}.")
-    print(f"Your current score is: {self.score}/{self.question_number}.")
-    print("\n")
-  
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+        self.score = 0
+
+    def still_has_questions(self):
+        # if self.question_number < len(self.question_list):
+        #     return True
+        # else:
+        #     return False
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_input = input(f"Q.{self.question_number}: {current_question.text} (True/False) ")
+        correct_answer = current_question.answer
+        self.check_answer(user_input, correct_answer)
+
+    def check_answer(self, user_input, correct_answer):
+        if user_input == correct_answer:
+            print("You are correct!!!!")
+            self.score += 1
+        else:
+            print("You are wrong")
+        print(f"The correct answer is: {type(correct_answer)}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
