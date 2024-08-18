32ca6d83cd440eb255951b7dc8e90e02fc4423f1
K-Musty <kmustapha9564@gmail.com>
2024-08-18 23:03:50 +0100

    Random walk: color mode included(r,g,b) palettes in random and saved to a tuple

--- 18-day/day-18-start/random-walk.py
+++ 18-day/day-18-start/random-walk.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
-from turtle import Turtle, Screen
+import turtle
+from turtle import Turtle, Screen, colormode
 import random
 tim = Turtle()
-tim.speed("fast")
+tim.speed("fastest")
 tim.pensize(5)
+turtle.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
 
 turns = [0, 90, 180, 270]
+
 for i in range(10000):
+    tim.color(random_color())
     steps = 20
     angle = int(random.choice(turns))
     tim.setheading(angle)
