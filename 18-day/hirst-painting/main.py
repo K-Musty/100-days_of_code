7e66701ec0ca0ac5d986d75733c94b8a265e0592
K-Musty <kmustapha9564@gmail.com>
2024-08-19 17:54:19 +0100

    Color gram

--- 18-day/hirst-painting/main.py
+++ 18-day/hirst-painting/main.py
@@ -1,8 +1,15 @@
-import colorgram
+# import colorgram
+#
+# colors = colorgram.extract('image.jpg', 20)
+#
+# cool_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     rgb = (r, g, b)
+#     cool_colors.append(rgb)
+#
+# print(cool_colors)
 
-colors = colorgram.extract('image.jpg', 20)
-
-color = colors[0]
-print(color)
-# for _ in range(20):
-#     first_color = colors[0]
+color_list = [(162, 168, 34), (242, 78, 54), (148, 50, 101), (239, 67, 131), (8, 144, 64), (230, 115, 164), (242, 219, 73), (4, 141, 182), (69, 198, 223), (161, 58, 50), (22, 171, 133), (120, 37, 85), (251, 231, 0), (25, 190, 210), (113, 195, 165)]
