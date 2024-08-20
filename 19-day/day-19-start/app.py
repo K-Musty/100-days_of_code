9005658084a49ab8aeec7917523657f71f95b87c
K-Musty <kmustapha9564@gmail.com>
2024-08-20 15:38:17 +0100

    Turtle race: Game logic completed

--- 19-day/day-19-start/app.py
+++ 19-day/day-19-start/app.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import turtle
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, TK
 import random
 
 is_race_on = False
 screen = Screen()
 screen.setup(500, 500)
-user_choice = screen.textinput("Make you bet", 'Which turtle would win the race: "red", "blue", "yellow", "orange", "pink", or "green"')
+user_choice = screen.textinput("Make you bet", 'Which turtle would win the race: pick a color')
 
 colors = ["red", "blue", "yellow", "orange", "pink", "green"]
 y_pos = [80, 40, 0, -40, -80, -120]
@@ -22,19 +22,17 @@ if user_choice:
     is_race_on = True
 
 while is_race_on:
-
-
     for turtle in turtle_list:
         if turtle.xcor() > 230:
             is_race_on = False
             winning_color = turtle.pencolor()
             if winning_color == user_choice:
                 print(f"You've won!!, the {winning_color} turtle won")
+                TK.messagebox.showinfo(title="You won", message="Hooray, you won")
             else:
                 print(f"You've lost, the {winning_color} turtle won ")
+                TK.messagebox.showinfo(title="You lost", message="Better luck next time")
         speed = random.randint(0, 10)
         turtle.forward(speed)
-
-
 
 screen.exitonclick()
