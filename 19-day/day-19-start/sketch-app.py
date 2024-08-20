515d68981e9c92a48b39d9bb1d932d34576f501a
K-Musty <kmustapha9564@gmail.com>
2024-08-20 10:53:26 +0100

    E-sketch App completed

--- 19-day/day-19-start/sketch-app.py
+++ 19-day/day-19-start/sketch-app.py
@@ -21,9 +21,16 @@ def turn_left():
     tim.left(10)
 
 
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
 screen.onkey(key="w", fun=move_forward)
 screen.onkey(key="d", fun=turn_right)
 screen.onkey(key="s", fun=move_backward)
 screen.onkey(key="a", fun=turn_left)
-
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
