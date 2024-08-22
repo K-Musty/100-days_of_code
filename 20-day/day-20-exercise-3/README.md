45c8060659d178785760fe6883ca7ac716bd079f
K-Musty <kmustapha9564@gmail.com>
2024-08-22 23:21:25 +0100

    Snake motion

---
+++ 20-day/day-20-exercise-3/README.md
@@ -0,0 +1,2 @@
+### Snake Game challenge 3 of day 20
+- Snake control
---
+++ 20-day/day-20-exercise-3/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(-40, 0), (-20, 0), (0, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+class Snake:
+
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for place in STARTING_POSITIONS:
+            body = Turtle("square")
+            body.color("white")
+            body.penup()
+            body.goto(place)
+            self.segments.append(body)
+
+    def move(self):
+        for seg in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg - 1].xcor()
+            new_y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
