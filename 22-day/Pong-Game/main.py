bab0029aab6931b1ace258faee412f7db4fc458f
K-Musty <kmustapha9564@gmail.com>
2024-08-28 15:25:37 +0100

    Pong Game: paddle and ball logic, class and main complete

--- 22-day/Pong-Game/main.py
+++ 22-day/Pong-Game/main.py
@@ -2,6 +2,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,7 +13,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((380, 0))
 l_paddle = Paddle((-380, 0))
-
+scoreboard = Scoreboard
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -21,9 +22,8 @@ screen.onkey(l_paddle.go_up, "w")
 screen.onkey(l_paddle.go_down, "s")
 
 ball = Ball()
-
 center = Turtle()
-center.goto(0,-320)
+center.goto(0, -320)
 center.hideturtle()
 center.hideturtle()
 center.color("white")
@@ -42,14 +42,13 @@ while game_is_on:
     screen.update()
     ball.move()
 
-    #Detection with wall
+    # Detection with wall
     if ball.ycor() > 240 or ball.ycor() < -240:
         ball.bounce_y()
 
-    #Detection with paddle
+    # Detection with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 350 or ball.distance(l_paddle) < 50 and ball.xcor() < -350:
         ball.bounce_x()
-
 
     if ball.xcor() > 380:
         ball.reset_position()
