28f33c14965d00a1e28d331d17acceec915a8a97
K-Musty <kmustapha9564@gmail.com>
2024-08-30 22:01:25 +0100

    Car Manager Class logic for turtle crossing nerly complete

--- 23-day/turtle-crossing-start/car_manager.py
+++ 23-day/turtle-crossing-start/car_manager.py
@@ -1,7 +1,18 @@
+from turtle import Turtle
+from random import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+
+    def __init__(self):
+        all_cars = []
+
+    def create_cars(self):
+        self.new_car = Turtle
+        self.new_car.shape("square")
+        self.new_car.penup()
+        self.new_car.color(COLORS[0])
+        self.new_car.shapesize(stretch_len=2, stretch_wid=1)
