b7e776ac84724ad1aefa8dadd8bcb037cb7709b8
K-Musty <kmustapha9564@gmail.com>
2024-08-31 22:29:29 +0100

    Car Manager class: implemented a method to create random cars and appended it to list

--- 23-day/turtle-crossing-start/car_manager.py
+++ 23-day/turtle-crossing-start/car_manager.py
@@ -1,5 +1,5 @@
 from turtle import Turtle
-from random import random
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
@@ -8,14 +8,16 @@ MOVE_INCREMENT = 10
 class CarManager:
 
     def __init__(self):
-        all_cars = []
-        for i in range(6):
-            self.create_cars()
-            all_cars.append(i)
+        self.all_cars = []
+
+
 
     def create_cars(self):
-        self.new_car = Turtle()
-        self.new_car.shape("square")
-        self.new_car.penup()
-        self.new_car.color(COLORS[0])
-        self.new_car.shapesize(stretch_len=2, stretch_wid=1)
+        new_car = Turtle()
+        new_car.shape("square")
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        new_car.shapesize(stretch_len=2, stretch_wid=1)
+        random_y = random.randint(-250, 250)
+        new_car.goto(300, random_y)
+        self.all_cars.append(new_car)
