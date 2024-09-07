036d243fbea66a4cbbb7179c8af6c7e26f12c63c
K-Musty <kmustapha9564@gmail.com>
2024-09-07 22:42:08 +0100

    Working with csv, and getting started with panda

---
+++ 25-day/day-25-start/main.py
@@ -0,0 +1,12 @@
+# with open("weather_data.csv", mode="r") as weather_data:
+#     data = weather_data.readlines()
+#     print(data)
+
+import csv
+
+with open("weather_data.csv") as weather_data:
+    data = csv.reader(weather_data)
+    temperature = []
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
