2c1f1a50bd9cfbae86741560aeb761ad850fd289
K-Musty <kmustapha9564@gmail.com>
2024-09-10 13:38:35 +0100

    States not guessed converted to csv for user to learn

--- 25-day/us-states-game-start/main.py
+++ 25-day/us-states-game-start/main.py
@@ -22,6 +22,17 @@ guessed_states = []
 while len(guessed_states) < 50:
 
     answer = screen.textinput(f"{len(guessed_states)}/50 states guessed correctly", "Name a state").title()
+
+    if answer == "Exit":
+        not_guessed = []
+        for state in data_states:
+            if state not in guessed_states:
+                not_guessed.append(state)
+        new_data = pandas.DataFrame(not_guessed)
+        new_data.to_csv("new_csv.csv")
+
+        break
+
     if answer in data_states:
         guessed_states.append(answer)
         tim = turtle.Turtle()
@@ -32,4 +43,3 @@ while len(guessed_states) < 50:
         tim.goto(int(attr.x.iloc[0]), int(attr.y.iloc[0]))
         tim.write(answer)
 
-turtle.mainloop()
