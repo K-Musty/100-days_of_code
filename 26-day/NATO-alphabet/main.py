0899166747a59941ec19788ebd6a8bb52d502637
K-Musty <kmustapha9564@gmail.com>
2024-09-11 22:10:47 +0100

    Day 26: NATO-PHONETICS completed

--- 26-day/NATO-alphabet/main.py
+++ 26-day/NATO-alphabet/main.py
@@ -21,7 +21,12 @@ for (index, row) in student_data_frame.iterrows():
 # {new_key:new_value for (index, row) in df.iterrows()}
 
 #TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
+# {"A": "Alfa", "B": "Bravo"}
+nato_phonetics = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_phonetics_dict = { row.letter: row.code for (index, row) in nato_phonetics.iterrows() }
+print(nato_phonetics_dict)
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("What is your name? ").upper()
 
+letters = [nato_phonetics_dict[letter] for letter in user_input]
+print(letters)
