54c1ca053c6611aec2191f446bdfaabb53f7df73
K-Musty <kmustapha9564@gmail.com>
2024-09-12 21:34:20 +0100

    Keywords arguments (**kwargs)

--- 27-day/playground.py
+++ 27-day/playground.py
@@ -1,8 +1,18 @@
-def add(*args):
-    sum = 0
-    for n in args:
-        sum += n
-    return sum
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum += n
+#     return sum
+#
+#
+# print(add(2, 5, 7, 8))
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for (key, value) in kwargs.items():
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
 
 
-print(add(2, 5, 7, 8))
+calculate(2, add=3, multiply=6)
