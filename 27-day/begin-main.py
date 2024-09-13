37b3d2543c35dd2ef926fd4a443b081ac94dcaa9
K-Musty <kmustapha9564@gmail.com>
2024-09-13 22:01:43 +0100

    TKinter widgets

---
+++ 27-day/begin-main.py
@@ -0,0 +1,35 @@
+from tkinter import *
+
+# Window
+window = Tk()
+window.title("My First GUI program")
+window.minsize(width=500, height=300)
+
+# Label
+my_label = Label(text="My Label", font=("Arial", 22, "bold"))
+my_label.pack()
+
+# Any of the below two can be used to change text
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+
+# Button
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
+my_button = Button(text="Click Me", command=button_clicked)
+my_button.pack()
+
+# Entry
+
+
+input = Entry(width=10)
+input.pack()
+
+
+
+window.mainloop()
