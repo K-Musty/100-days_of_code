e8e2534b2ea5967537a54f74fd39f5b9b06c8929
K-Musty <kmustapha9564@gmail.com>
2024-09-15 22:03:57 +0100

    Day 27: Tkinker

--- 27-day/begin-main.py
+++ 27-day/begin-main.py
@@ -4,10 +4,18 @@ from tkinter import *
 window = Tk()
 window.title("My First GUI program")
 window.minsize(width=500, height=300)
+window.config(padx=25, pady=25)
+
+
+def button_clicked():
+    print("I got clicked")
+    new_text = input.get()
+    my_label.config(text=new_text)
+
 
 # Label
 my_label = Label(text="My Label", font=("Arial", 22, "bold"))
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 # Any of the below two can be used to change text
 my_label["text"] = "New Text"
@@ -15,21 +23,15 @@ my_label.config(text="New Text")
 
 
 # Button
-def button_clicked():
-    print("I got clicked")
-    new_text = input.get()
-    my_label.config(text=new_text)
-
 
 my_button = Button(text="Click Me", command=button_clicked)
-my_button.pack()
-
+my_button.grid(column=1, row=1)
+new_button = Button(text="New button", command=button_clicked)
+new_button.grid(column=2, row=0)
 # Entry
 
-
 input = Entry(width=10)
-input.pack()
-
+input.grid(column=3, row=3)
 
 
 window.mainloop()
