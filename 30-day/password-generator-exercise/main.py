9213481b338e0feb4671a46bd6e6551215840cc1
K-Musty <kmustapha9564@gmail.com>
2024-09-20 21:38:58 +0100

    Day 30: Error Handling and Json r,w,u completed

--- 30-day/password-generator-exercise/main.py
+++ 30-day/password-generator-exercise/main.py
@@ -3,6 +3,7 @@ from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
 import json
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -113,6 +114,5 @@ add_button = Button(text="Add", width=39, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 search_button = Button(text="Search", width=14, command=search)
 search_button.grid(row=1, column=2)
-
 
 window.mainloop()
