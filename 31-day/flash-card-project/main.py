02bf3358d6bdf12a5cfb7615c265e294c9a0b72a
K-Musty <kmustapha9564@gmail.com>
2024-09-22 18:11:07 +0100

    DAY 31 exercise 1 completed: UI design

--- 31-day/flash-card-project/main.py
+++ 31-day/flash-card-project/main.py
@@ -1,2 +1,25 @@
+from tkinter import *
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Flash Card")
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+
+# Canvas
+canvas = Canvas(width=800, height=526)
+card_front = PhotoImage(file="images/card_front.png")
+card_back = PhotoImage(file="images/card_back.png")
+canvas.create_image(400, 263, image=card_front)
+canvas.create_text(400, 150, text="Title", font=("Arial", 20, "italic"))
+canvas.create_text(400, 263, text="content of flash card", font=("Arial", 20, "italic"))
+canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2)
+# Buttons
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(column=1, row=1)
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(column=0, row=1)
+
+window.mainloop()
