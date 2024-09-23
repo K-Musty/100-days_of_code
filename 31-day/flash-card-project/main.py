9789279fd8a9ceeb92c43509a6ecc5c1cedce096
K-Musty <kmustapha9564@gmail.com>
2024-09-23 10:47:39 +0100

    Day 31: Flash Card Application completed

--- 31-day/flash-card-project/main.py
+++ 31-day/flash-card-project/main.py
@@ -1,19 +1,27 @@
 from tkinter import *
+from tkinter import messagebox
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
-
-panda_data = pandas.read_csv("data/french_words.csv")
-# stuff = pandas.DataFrame(french_data)
-# french_list = stuff["French"]
-data = panda_data.to_dict(orient="records")
+data = {}
 random_word = {}
+try:
+    panda_data = pandas.read_csv("data/words_to_learn.csv")
+    # stuff = pandas.DataFrame(french_data)
+    # french_list = stuff["French"]
+except pandas.errors.EmptyDataError:
+    original_panda_data = pandas.read_csv("data/french_words.csv",)
+    data = original_panda_data.to_dict(orient="records")
+else:
+    data = panda_data.to_dict(orient="records")
 
 
 def next_cards():
     global random_word, flip_timer
     window.after_cancel(flip_timer)
     random_word = random.choice(data)
+    if len(random_word) == 0:
+        messagebox.showinfo(title="Alert", message="You've exhausted all words")
     canvas.itemconfig(word, text=random_word["French"], fill="black")
     canvas.itemconfig(title, text="French", fill="black")
     flip_timer = window.after(3000, show_english)
@@ -25,6 +33,12 @@ def show_english():
     canvas.itemconfig(word, text=random_word["English"], fill="white")
     canvas.itemconfig(title, text="English", fill="white")
 
+
+def known_cards():
+    data.remove(random_word)
+    learning_data = pandas.DataFrame(data)
+    learning_data.to_csv("data/words_to_learn.csv", index=False)
+    next_cards()
 # ---------------------------- UI -------------------------------------- #
 
 
@@ -45,7 +59,7 @@ canvas.config(background=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 # Buttons
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=next_cards)
+right_button = Button(image=right_image, highlightthickness=0, command=known_cards)
 right_button.grid(column=1, row=1)
 wrong_image = PhotoImage(file="images/wrong.png")
 wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_cards)
