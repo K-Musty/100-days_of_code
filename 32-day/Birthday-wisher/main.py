6623aa85e921600502a4047326b693f0417bc52b
K-Musty <kmustapha9564@gmail.com>
2024-09-28 22:20:19 +0100

    Date time module

--- 32-day/Birthday-wisher/main.py
+++ 32-day/Birthday-wisher/main.py
@@ -8,24 +8,29 @@
 # connection.login(user=my_email, password=password)
 # connection.sendmail(from_addr=my_email, to_addrs="lemonpart@yahoo.com", msg="Hello")
 # connection.close()
+# import smtplib
+#
+# # Your email and app-specific password (if 2FA is enabled)
+# my_email = "johnnymont2255@gmail.com"
+# password = "ayrghhhjgmcenzfh"  # Use the App Password generated from your Google account
+#
+# # Connect to Gmail's SMTP server using SSL on port 465
+# connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+#
+# # Log in to your Gmail account
+# connection.login(user=my_email, password=password)
+#
+# # Send an email
+# connection.sendmail(
+#     from_addr=my_email,
+#     to_addrs="lemonpart@yahoo.com",  # Recipient's email address
+#     msg="Subject:Test\n\nHello, this is a test email."  # Ensure correct email format with subject and body
+# )
+#
+# # Close the connection
+# connection.close()
 import smtplib
-
-# Your email and app-specific password (if 2FA is enabled)
-my_email = "johnnymont2255@gmail.com"
-password = "ayrghhhjgmcenzfh"  # Use the App Password generated from your Google account
-
-# Connect to Gmail's SMTP server using SSL on port 465
-connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-
-# Log in to your Gmail account
-connection.login(user=my_email, password=password)
-
-# Send an email
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs="lemonpart@yahoo.com",  # Recipient's email address
-    msg="Subject:Test\n\nHello, this is a test email."  # Ensure correct email format with subject and body
-)
-
-# Close the connection
-connection.close()
+import datetime as dt
+now = dt.datetime.now()
+year = now.year
+print(year)
