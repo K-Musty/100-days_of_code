eb6db7284d016729db1d9ff926c033e77fb379f5
K-Musty <kmustapha9564@gmail.com>
2024-09-30 21:07:24 +0100

    Day 32: Email SMTP

---
+++ 32-day/Monday_motivation/main.py
@@ -0,0 +1,42 @@
+# import smtplib
+#
+# my_email = "johnnymont2255@gmail.com"
+# password = "ayrghhhjgmcenzfh"
+#
+# connection = smtplib.SMTP("smtp.gmail.com", 465)
+# connection.starttls()
+# connection.login(user=my_email, password=password)
+# connection.sendmail(from_addr=my_email, to_addrs="lemonpart@yahoo.com", msg="Hello")
+# connection.close()
+# import smtplib
+#
+# # Your email and app-specific password (if 2FA is enabled)
+# my_email = "johnnymont2255@gmail.com"
+# password = "ayrghhhjgmcenzfh"  # Use the App Password generated from your Google account
+#
+# # Connect to Gmail's SMTP server using SSL on port 465
+# connection = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+#
+# # Log in to your Gmail account
+# connection.login(user=my_email, password=password)
+#
+# # Send an email
+# connection.sendmail(
+#     from_addr=my_email,
+#     to_addrs="lemonpart@yahoo.com",  # Recipient's email address
+#     msg="Subject:Test\n\nHello, this is a test email."  # Ensure correct email format with subject and body
+# )
+#
+# # Close the connection
+# connection.close()
+import smtplib
+import datetime as dt
+now = dt.datetime.now()
+month = now.month
+year = now.year
+day_of_week = now.weekday()
+print(day_of_week)
+print(year)
+
+date_of_birth = dt.datetime(day=25, month=4, year=1999,)
+print(date_of_birth)
