d66c204a3ec874905b2b59a6a04f841ffedd9325
K-Musty <kmustapha9564@gmail.com>
2024-10-08 20:38:19 +0100

    Day 33: Quiz app User Interface

--- 34-day/quizler-app/ui.py
+++ 34-day/quizler-app/ui.py
@@ -1,2 +1,24 @@
+from tkinter import *
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=100, pady=100, background=THEME_COLOR)
+
+        self.score_label = Label()
+        self.score_label.config(text="Score")
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=200, height=200, background="white", highlightthickness=0)
+        self.canvas.create_window(width=200, height=200)
+        self.canvas.grid(column=1, row=1, coloumnspan=2)
+        self.right_answer = Button(padx=25, pady=25)
+        self.right_answer.grid(row=1, column=0,)
+        self.wrong_answer = Button(padx=25, pady=25)
+        self.wrong_answer.grid(row=1, column=1)
+
+
+        self.window.mainloop()
