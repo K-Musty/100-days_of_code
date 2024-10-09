c5ab86265d966ef2bd1b6f96f3a945f20d661388
K-Musty <kmustapha9564@gmail.com>
2024-10-09 23:35:14 +0100

    Day 33: Tkinker GUI implementation

--- 34-day/quizler-app/ui.py
+++ 34-day/quizler-app/ui.py
@@ -19,11 +19,11 @@ class QuizInterface:
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         # Buttons
-        right_image = PhotoImage(file="images/true.png", width=100, height=100)
-        self.right_answer = Button(padx=25, pady=25, background=right_image)
+        self.right_image = PhotoImage(file="images/true.png")
+        self.right_answer = Button(width=100, height=100, image=self.right_image, highlightthickness=0)
         self.right_answer.grid(row=2, column=0)
-        wrong_image = PhotoImage(file="images/false.png", width=100, height=100)
-        self.wrong_answer = Button(padx=25, pady=25, background=wrong_image)
+        self.wrong_image = PhotoImage(file="images/false.png")
+        self.wrong_answer = Button(width=100, height=100, image=self.wrong_image, highlightthickness=0)
         self.wrong_answer.grid(row=2, column=1)
 
         self.window.mainloop()
