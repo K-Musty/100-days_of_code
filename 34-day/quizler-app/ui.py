dfdf11a4804e160ee25085c4687d403f16b0b943
K-Musty <kmustapha9564@gmail.com>
2024-10-10 22:26:52 +0100

    Day 33: get_next_question() method

--- 34-day/quizler-app/ui.py
+++ 34-day/quizler-app/ui.py
@@ -1,13 +1,12 @@
 from tkinter import *
 THEME_COLOR = "#375362"
-from data import question_data
+from quiz_brain import QuizBrain
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         # Window
-        def next_question():
-            self.canvas.itemconfig(self.question_text, text=f"{question_data}")
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=100, pady=100, background=THEME_COLOR)
@@ -17,19 +16,24 @@ class QuizInterface:
 
         # Canvas for displaying questions
         self.canvas = Canvas(width=300, height=250, background="white", highlightthickness=0)
-        self.question_text = self.canvas.create_text(150, 125, text="Question goes here ", width=280, fill=THEME_COLOR,
+        self.question_text = self.canvas.create_text(150, 125,
+                                                     width=280,
+                                                     text="Question goes here ", fill=THEME_COLOR,
                                                      font=("Arial", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         # Buttons
         self.right_image = PhotoImage(file="images/true.png")
-        self.right_answer = Button(width=100, height=100, image=self.right_image, highlightthickness=0, command=next_question)
+        self.right_answer = Button(width=100, height=100, image=self.right_image, highlightthickness=0)
         self.right_answer.grid(row=2, column=0)
         self.wrong_image = PhotoImage(file="images/false.png")
         self.wrong_answer = Button(width=100, height=100, image=self.wrong_image, highlightthickness=0)
         self.wrong_answer.grid(row=2, column=1)
 
+        self.get_next_question()
 
-        def next_question():
-            self.canvas.itemconfig(self.question_text, text=f"{question_data}")
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
