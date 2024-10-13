c378cbe221733e82615e84cc8f5955f2d32bbea4
K-Musty <kmustapha9564@gmail.com>
2024-10-13 23:08:57 +0100

    Day 34: Python typing type Hints and Arrows

---
+++ 34-day/main.py
@@ -0,0 +1,15 @@
+# Python Typing type Hints and Arrows
+
+# age: int
+# name: str
+# height: float
+# is_human: bool
+
+def police_check(age):
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+polive_check(12)
