07342cedbe72aa9a84342994ed5e622489f130b7
K-Musty <kmustapha9564@gmail.com>
2024-10-16 21:35:11 +0100

    Day 35: Can't access the free version of the one call API so i generated one using Chatgpt to help me finish the Rain Alert app

--- 35-day/rain-alert/main.py
+++ 35-day/rain-alert/main.py
@@ -1,10 +1,15 @@
 import requests
-api_key = "f6f9c5f85cdcd261168cdd9340fdacca"
-parameters =  {
-    "lat": 11.999970,
-    "lon": 8.534860,
-    "appid": api_key
-}
-response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
-data = response.json()
-print(data)
+import json
+# api_key = "f6f9c5f85cdcd261168cdd9340fdacca"
+# parameters =  {
+#     "lat": 11.999970,
+#     "lon": 8.534860,
+#     "appid": api_key,
+#     "exclude": "minutely,
+# }
+# response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", params=parameters)
+# data = response.json()
+with open("weather.json") as file:
+    data = json.load(file)
+weather_data = data["hourly"][0]["weather"][0]["id"]
+print(weather_data)
