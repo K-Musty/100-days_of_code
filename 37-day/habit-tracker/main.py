0825518113484d84fba84d9b7f9b4e6ca3ae4500
K-Musty <kmustapha9564@gmail.com>
2024-10-22 21:19:34 +0100

    Day 37: Habit tracker update and delete included

--- 37-day/habit-tracker/main.py
+++ 37-day/habit-tracker/main.py
@@ -24,14 +24,29 @@ headers = {
     "X-USER-TOKEN": TOKEN,
 }
 # graph_response = requests.post(url=graph_endpoint, json=graph_parameters, headers=headers)
-# posting
+# ----------------------------------------------- posting ------------------------------------#
 today = datetime.now()
 date_of = today.strftime("%Y%m%d")
 post_endpoint = f"{graph_endpoint}/graph1"
 post_parameters = {
     "date": date_of,
-    "quantity": "6"
+    "quantity": input('How many times did you code today?  ')
 }
-
+#
 post_response = requests.post(url=post_endpoint, json=post_parameters, headers=headers)
 print(post_response.text)
+# -------------------------Updating Pixel using PUT request-----------------------------------------------------#
+
+update_endpoint = f"{post_endpoint}/{date_of}"
+update_parameters = {
+    "quantity": "9"
+}
+
+# update_response = requests.put(url=update_endpoint, json=update_parameters, headers=headers)
+# print(update_response.text)
+
+# --------------------------------------Deleting a pixel using DELETE request ---------------------------------------------------------------#
+delete_endpoint = f"{post_endpoint}/{date_of}"
+
+# delete_response = (requests.delete(url=delete_endpoint, headers=headers))
+# print(delete_response.text)
