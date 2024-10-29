50bee0dbf88ad52dde72944446a7bbf96a8bb1bc
K-Musty <kmustapha9564@gmail.com>
2024-10-29 23:08:22 +0100

    Day 39: Updating Google Sheets with Testing instead of blank using the PUT request

--- 39-day/Flight-Deals/flight_search.py
+++ 39-day/Flight-Deals/flight_search.py
@@ -3,11 +3,7 @@ class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
     def __init__(self):
         pass
-        # self.cheap_api = "https://kiwi-com-cheap-flights.p.rapidapi.com/round-trip"
-        # self.headers = {
-        #     "x-rapidapi-key": "fd92ea0d33msh429fef1462a55cdp14a0c9jsnf9852713306a",
-        #     "x-rapidapi-host": "kiwi-com-cheap-flights.p.rapidapi.com"
-        # }
+
 
     def get_iata_code(self, city_name):
         # For now, just return "TESTING" for all city names
