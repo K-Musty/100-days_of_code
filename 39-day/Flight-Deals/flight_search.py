a432401e93c943574331a9952248ebb2bc317cd6
K-Musty <kmustapha9564@gmail.com>
2024-10-31 23:59:05 +0100

    Day 38: still trying to get IATA coe

--- 39-day/Flight-Deals/flight_search.py
+++ 39-day/Flight-Deals/flight_search.py
@@ -1,23 +1,23 @@
 import requests
 class FlightSearch:
     #This class is responsible for talking to the Flight Search API.
-    def __init__(self):
-        pass
-
+    def __init__(self, api_key):
 
     def get_iata_code(self, city_name):
-        # For now, just return "TESTING" for all city names
+        aviation_params = {
+            "access_key": self.api_key,
+            "search": city_name
+        }
+        response = requests.get(self.aviation_api, params=aviation_params)
+        aviation_data = response.json()
+        print(aviation_data)
 
-
-
-
-
-
-
-
-
-
-        return "TESTING"
+        if "data" in aviation_data and len(aviation_data["data"]) > 0:
+            for entry in aviation_data["data"]:
+                print(entry)
+                if entry["city"].lower() == city_name.lower() and entry["iata_code"]:
+                    return entry["iata_code"]
+        return None
 
     # def get_flight(self):
     #     response = requests.get(url=self.cheap_api, headers=self.headers)
