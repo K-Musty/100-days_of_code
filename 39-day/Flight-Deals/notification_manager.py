1e3699d4b5bd16cc894374aaaf712e8074739d8d
K-Musty <kmustapha9564@gmail.com>
2024-11-01 23:46:10 +0100

    Day 39: Sending message with twillo

--- 39-day/Flight-Deals/notification_manager.py
+++ 39-day/Flight-Deals/notification_manager.py
@@ -1,3 +1,17 @@
+from twilio.rest import Client
+SID = ""
+AUTH_TOKEN = ""
+
 class NotificationManager:
     #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(SID, AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=,
+            to=,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
