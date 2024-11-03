adba139ad8ff5e5d056a2b5879fba9a2a10a0755
K-Musty <kmustapha9564@gmail.com>
2024-11-03 23:50:43 +0100

    Day 40: Creating Basic questions

--- 40-day/Flight-Club/main.py
+++ 40-day/Flight-Club/main.py
@@ -1,1 +1,12 @@
-# user authentication with sheety
+print("Welcome to KMusty's Flight Club !!\n"
+      "We find the best flight deals and email you")
+
+first_name = input("Insert your first name")
+last_name = input("Insert your last name")
+while True:
+    first_email = input("Insert your email")
+    second_email = input("please insert your email for confirmation")
+
+    if first_email == second_email:
+        print("Your now in the club !!!")
+        break
