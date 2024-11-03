d55c34671726b872d72f5bc9ba048c1f03c8cca3
K-Musty <kmustapha9564@gmail.com>
2024-11-03 23:53:53 +0100

    Day 40: Creating Basic questions for user firstname, lastname and email

--- 40-day/Flight-Club/main.py
+++ 40-day/Flight-Club/main.py
@@ -1,12 +1,14 @@
 print("Welcome to KMusty's Flight Club !!\n"
       "We find the best flight deals and email you")
 
-first_name = input("Insert your first name")
-last_name = input("Insert your last name")
+first_name = input("Insert your first name\n")
+last_name = input("Insert your last name\n")
 while True:
-    first_email = input("Insert your email")
-    second_email = input("please insert your email for confirmation")
+    first_email = input("Insert your email\n")
+    second_email = input("please insert your email for confirmation\n")
 
     if first_email == second_email:
         print("Your now in the club !!!")
         break
+    else:
+        print("Email doesn't match try again")
