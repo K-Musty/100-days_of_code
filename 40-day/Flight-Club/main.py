154a9d4abbe8754c776593b48c284f6a9e54a684
K-Musty <kmustapha9564@gmail.com>
2024-11-04 22:18:44 +0100

    Day 39: Posting user data to google sheet

--- 40-day/Flight-Club/main.py
+++ 40-day/Flight-Club/main.py
@@ -1,14 +1,26 @@
-print("Welcome to KMusty's Flight Club !!\n"
+import requests
+
+
+print("Welcome to K-Musty's Flight Club !!\n"
       "We find the best flight deals and email you")
 
-first_name = input("Insert your first name\n")
-last_name = input("Insert your last name\n")
+first_name = input("Insert your first name \n")
+last_name = input("Insert your last name \n")
 while True:
-    first_email = input("Insert your email\n")
+    first_email = input("Insert your email \n")
     second_email = input("please insert your email for confirmation\n")
 
     if first_email == second_email:
+        parameters = {
+            "user": {
+                "First Name": first_name,
+                "Last Name": last_name,
+                "Email": first_email
+            }
+        }
+        response = requests.post(url=url, json=parameters, headers=headers)
+        # print(response.status_code)
         print("Your now in the club !!!")
         break
     else:
-        print("Email doesn't match try again")
+        print("Email doesn't match try again\n")
