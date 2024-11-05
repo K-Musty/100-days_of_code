3378cbe0cd28b9cf78d4369c2f5039c661d791a8
K-Musty <kmustapha9564@gmail.com>
2024-11-05 23:40:19 +0100

    Day 40: Getting codes from flight deals project in Day 39

---
+++ 40-day/data_manager.py
@@ -0,0 +1,30 @@
+import requests
+from flight_search import FlightSearch
+
+
+class DataManager:
+    #This class is responsible for talking to the Google Sheet.
+
+
+    def get_data(self):
+        response =  requests.get(url=self.api_sheety, headers=self.headers)
+        print(response.text)
+
+    def update_iata(self, sheet):
+        flight_search = FlightSearch(api_key="")
+
+        for item in sheet["prices"]:
+            if item["iataCode"] == "":
+                city_name = item["city"]
+                row_id = item["id"]
+                put_api = f"{self.api_sheety}/{row_id}"
+                iata_code = flight_search.get_iata_code(city_name)
+                parameters = {
+                    "price": {
+                        "iataCode": iata_code
+                    }
+                }
+
+                put_response = requests.put(url=put_api, json=parameters, headers=self.headers)
+                print(put_response.text)
+        return sheet
