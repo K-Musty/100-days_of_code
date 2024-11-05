bd08b5c0441cb1f333dcd5562d591ea72a7ae290
K-Musty <kmustapha9564@gmail.com>
2024-11-05 23:43:24 +0100

    Day 40: Getting codes from flight deals project in Day 39

---
+++ 40-day/notification_manager.py
@@ -0,0 +1,19 @@
+from twilio.rest import Client
+SID = ""
+AUTH_TOKEN = ""
+
+
+
+class NotificationManager:
+    #This class is responsible for sending notifications with the deal flight details.
+    def __init__(self):
+        self.client = Client(SID, AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=,
+            to=,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
