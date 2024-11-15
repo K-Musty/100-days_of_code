87142cad4134b6df60ea5a07c9e3cddb0380674c
K-Musty <kmustapha9564@gmail.com>
2024-11-15 23:34:43 +0100

    Day 45: Scraping data using 'find, find_all, select, select_one' to capture specific entities

--- 45-day/bs4/main.py
+++ 45-day/bs4/main.py
@@ -3,4 +3,22 @@ from bs4 import BeautifulSoup
 with open("website.html", mode="r") as file:
     data = file.read()
 soup = BeautifulSoup(data, "html.parser")
-print(soup.title.string)
+# print(soup.title)
+# print(soup.title.string)
+anchor_tags = soup.find_all(name="a")
+# for tag in anchor_tags:
+#     print(tag.get("href"))
+
+print(anchor_tags)
+
+h1 = soup.find(name="h1", id="name")
+print(h1.string)
+
+heading = soup.find(name="h3", class_="heading")
+print(heading.getText())
+
+company_url = soup.select_one(selector="#name")
+print(company_url)
+
+headings = soup.select(".heading")
+print(headings)
