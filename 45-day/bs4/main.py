1b978ab23002b30539f091ddcd5b5e2a3898751a
K-Musty <kmustapha9564@gmail.com>
2024-11-16 22:26:48 +0100

    Day 45: Scraping Live data from https:news.ycombinator.com/news

--- 45-day/bs4/main.py
+++ 45-day/bs4/main.py
@@ -1,24 +1,59 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html", mode="r") as file:
-    data = file.read()
-soup = BeautifulSoup(data, "html.parser")
-# print(soup.title)
-# print(soup.title.string)
-anchor_tags = soup.find_all(name="a")
-# for tag in anchor_tags:
-#     print(tag.get("href"))
+response = requests.get("https://news.ycombinator.com/news")
+yc_webpage = response.text
+# print(yc_webpage)
+soup = BeautifulSoup(yc_webpage, "html.parser")
+article = soup.find(id="42157335")
+article_select = article.select_one(".titleline")
+print(article_select.select_one("a").getText())
+print(article.getText())
 
-print(anchor_tags)
 
-h1 = soup.find(name="h1", id="name")
-print(h1.string)
 
-heading = soup.find(name="h3", class_="heading")
-print(heading.getText())
 
-company_url = soup.select_one(selector="#name")
-print(company_url)
 
-headings = soup.select(".heading")
-print(headings)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# with open("website.html", mode="r") as file:
+#     data = file.read()
+# soup = BeautifulSoup(data, "html.parser")
+# # print(soup.title)
+# # print(soup.title.string)
+# anchor_tags = soup.find_all(name="a")
+# # for tag in anchor_tags:
+# #     print(tag.get("href"))
+#
+# print(anchor_tags)
+#
+# h1 = soup.find(name="h1", id="name")
+# print(h1.string)
+#
+# heading = soup.find(name="h3", class_="heading")
+# print(heading.getText())
+#
+# company_url = soup.select_one(selector="#name")
+# print(company_url)
+#
+# headings = soup.select(".heading")
+# print(headings)
