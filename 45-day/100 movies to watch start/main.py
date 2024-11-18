15ad6755139e6e70c0440d68e6ca0d1609f11338
K-Musty <kmustapha9564@gmail.com>
2024-11-18 23:10:17 +0100

    Day 45: 100-movies to watch project completed

--- 45-day/100 movies to watch start/main.py
+++ 45-day/100 movies to watch start/main.py
@@ -5,4 +5,17 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL, )
 
+soup = BeautifulSoup(response.text, "html.parser")
+movie_list = []
+movies = soup.find_all(name="h3", class_="title")
+for movie in movies:
+    movie_list.append(movie.getText())
+
+# print(movie_list)
+reversed_list = movie_list[::-1]
+# print(reversed_list)
+with open("movies.txt", mode="w") as movies_file:
+    for i in reversed_list:
+        movies_file.writelines(f"{i}\n")
