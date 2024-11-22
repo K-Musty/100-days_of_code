3e3b0af6f2749f24c14670622b0fad4a8bd3e9ca
K-Musty <kmustapha9564@gmail.com>
2024-11-22 23:22:32 +0100

    Day 46: Musical time machine completed

--- 46-day/time-machine-playlist/main.py
+++ 46-day/time-machine-playlist/main.py
@@ -1,29 +1,22 @@
+from logging import raiseExceptions
+from pprint import pprint
+
 from bs4 import BeautifulSoup
 import requests
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from pprint import pprint
 
 
-
-
+SPOTIPY_CLIENT_ID="018be967ad924961bbdb2582e1a2fab0"
+SPOTIPY_CLIENT_SECRET="e30b77dc34ef4f1680c55e966d0ef05a"
+SPOTIPY_REDIRECT_URI="http://example.com"
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
                                                client_secret=SPOTIPY_CLIENT_SECRET,
                                                redirect_uri=SPOTIPY_REDIRECT_URI,
                                                scope="playlist-modify-private",
                                                cache_path="../token.txt",
                                                show_dialog=True))
-USER_ID = sp.current_user()["id"]
-PLAYLIST_NAME = "Time Machine Playlist"
-PLAYLIST_DESCRIPTION = "Takes top 100 music from date in past"
-new_playlist = sp.user_playlist_create(
-    user=USER_ID,
-    name=PLAYLIST_NAME,
-    public=False,
-    description=PLAYLIST_DESCRIPTION
-)
-
-print(f"Playlist '{PLAYLIST_NAME}' created successfully! Playlist ID: {new_playlist['id']}")
-
 
 travel_year = input("What year you would like to travel to in YYYY-MM-DD format?  ")
 
@@ -41,18 +34,33 @@ song_names = songs[6:403:4]
 # print(len(song_names))
 # print(song_names)
 
+songs_uris = []
 
+for song_name in song_names:
+    query = f"track: {song_name} year: {year}"
+    try:
+        new_playlist = sp.search(q=query, type="track")
+        songs_uris.append(new_playlist['tracks']['items'][0]['uri'])
+    except IndexError:
+        print(f"{song_name} does not exist")
 
+pprint(songs_uris)
 
+USER_ID = sp.current_user()["id"]
+PLAYLIST_NAME = f"{year} Billboard 100"
+PLAYLIST_DESCRIPTION = "Takes top 100 music from date in past"
+new_playlist = sp.user_playlist_create(
+    user=USER_ID,
+    name=PLAYLIST_NAME,
+    public=False,
+    description=PLAYLIST_DESCRIPTION
+)
 
+print(f"Playlist '{PLAYLIST_NAME}' created successfully! Playlist ID: {new_playlist['id']}")
+print(new_playlist)
 
-
-
-
-
-
-
-
+sp.playlist_add_items(playlist_id=new_playlist["id"], items=songs_uris)
+print("successful!!!")
 
 
 
