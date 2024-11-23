35c58de427c13eb28bd1f854d8a1b09047f0898c
K-Musty <kmustapha9564@gmail.com>
2024-11-23 22:25:32 +0100

    Day 47: Amazon Price Tracker

---
+++ 47-day/main.py
@@ -0,0 +1,10 @@
+import requests
+# import lxml
+
+URL="https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+headers= {
+
+}
+response = requests.get(url=URL, headers=headers)
+
+print(response.text)
