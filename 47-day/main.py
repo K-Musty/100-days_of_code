e6a70d3b22a509e627b3384982d3431c962358db
K-Musty <kmustapha9564@gmail.com>
2024-11-24 21:32:54 +0100

    Day 47: Scraping data from amazon; adding more parameters to headers and using lxml to parse

--- 47-day/main.py
+++ 47-day/main.py
@@ -1,10 +1,20 @@
 import requests
-# import lxml
+from bs4 import BeautifulSoup
+import lxml
+# from fake_useragent import UserAgent
 
 URL="https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 headers= {
-
+    "User-Agent": "",
+    "Accept-Language": "",
+    "Accept": "",
+    "Connection": "keep-alive",
+    "Referer": "https://www.google.com/",
+    "Accept-Encoding": ""
 }
 response = requests.get(url=URL, headers=headers)
+# print(response.text)
 
-print(response.text)
+soup = BeautifulSoup(response.text, "lxml")
+price = soup.find(name="span", class_="a-price-whole")
+print(price.prettify())
