5b6427c2cfee795d35492c410fb5cb609bc55d37
K-Musty <kmustapha9564@gmail.com>
2024-11-26 22:35:02 +0100

    Day 48: Using Service class to use the service argument in the webdriver object, 'executable_path' has been deprecated

--- 48-day/main.py
+++ 48-day/main.py
@@ -1,1 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+
+service = Service("/usr/local/bin/chromedriver")
+driver = webdriver.Chrome(service=service)
