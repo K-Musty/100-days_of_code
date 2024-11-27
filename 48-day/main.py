3bf6b556fee2896d432ce90358479be93b9bcf62
K-Musty <kmustapha9564@gmail.com>
2024-11-27 22:09:39 +0100

    Day 47: Amazon Price Tracker completed

--- 48-day/main.py
+++ 48-day/main.py
@@ -8,7 +8,7 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
 
-price = driver.find_element(id, "priceblock_ourprice")
+price = driver.find_element(by="class", value="a-price-whole")
 print(price.text)
 
 # driver.close()
