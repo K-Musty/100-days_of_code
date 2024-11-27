3e951b1050b8ad88fc08c781953531bd81dcbd62
K-Musty <kmustapha9564@gmail.com>
2024-11-27 21:58:06 +0100

    Day 48: Getting hold of elements using selenium

--- 48-day/main.py
+++ 48-day/main.py
@@ -5,3 +5,11 @@ from selenium.webdriver.chrome.service import Service
 
 service = Service("/usr/local/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
+
+driver.get("https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6")
+
+price = driver.find_element(id, "priceblock_ourprice")
+print(price.text)
+
+# driver.close()
+driver.quit()
