a62b9d4eebbbf6e2e542f51922724cfb45c81f59
K-Musty <kmustapha9564@gmail.com>
2024-11-29 23:50:39 +0100

    Day 48: Challenge 1; to get event dates and names and store them in a dictionary

--- 48-day/day-48-1-exercise.py
+++ 48-day/day-48-1-exercise.py
@@ -1,6 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 service = Service("/usr/local/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
 
+driver.get("https://www.python.org/")
+events = {}
+
+event_name = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+
+event_date = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+for i in range(len(event_date)):
+    events[i] = {
+        "time": f"2024-{event_date[i].text}",
+        "name": event_name[i].text
+    }
+
+
+print(events)
+
+driver.quit()
