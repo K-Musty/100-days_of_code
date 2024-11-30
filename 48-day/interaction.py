97965e57f169e7a32d9cc074d277c819713b1a22
K-Musty <kmustapha9564@gmail.com>
2024-11-30 21:20:28 +0100

    Day 48: Scraping Wikipedia to get number of article counts

--- 48-day/interaction.py
+++ 48-day/interaction.py
@@ -3,9 +3,11 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 
 
-service = Service("usr/local/bin/chromedriver")
+service = Service("/usr/local/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
+article_count = driver.find_element(By.ID, value="articlecount")
+print(article_count.text.split()[0])
 driver.quit()
