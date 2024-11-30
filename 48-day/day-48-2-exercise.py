2026a94167207c350d8c939226ec4eaabf178c44
K-Musty <kmustapha9564@gmail.com>
2024-11-30 23:54:37 +0100

    Day 48: Scraping Wikipedia to get number of article counts

---
+++ 48-day/day-48-2-exercise.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+
+service = Service("/usr/local/bin/chromedriver")
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+
+article_count = driver.find_element(By.ID, value="articlecount")
+article_count.click()
+print(article_count.text.split()[0])
+# driver.quit()
--- 48-day/interaction.py
+++ 48-day/interaction.py
@@ -9,5 +9,6 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
 article_count = driver.find_element(By.ID, value="articlecount")
+article_count.click()
 print(article_count.text.split()[0])
-driver.quit()
+# driver.quit()
