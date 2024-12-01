10b7d774cdafd4c6d594e89c55235131a11391bd
K-Musty <kmustapha9564@gmail.com>
2024-12-01 14:13:30 +0100

    Day 48: Inputing text using selenium and commanding the 'ENTER' key

--- 48-day/interaction.py
+++ 48-day/interaction.py
@@ -1,6 +1,9 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
 
 
 service = Service("/usr/local/bin/chromedriver")
@@ -8,7 +11,17 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-article_count = driver.find_element(By.ID, value="articlecount")
-article_count.click()
-print(article_count.text.split()[0])
+article_count = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+# article_count.click()
+
+comm_portal = driver.find_element(By.PARTIAL_LINK_TEXT, value="Community portal")
+# comm_portal.click()
+
+search = driver.find_element(By.NAME, value="search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
+
+time.sleep(10)
+
+# print(article_count.text.split()[0])
 # driver.quit()
