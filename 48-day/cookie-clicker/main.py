8f60242265145d2ff0ad14f02305186330ea7f5f
K-Musty <kmustapha9564@gmail.com>
2024-12-03 23:23:59 +0100

    Day 48: Scraping Data for 'money', and 'right panel'

--- 48-day/cookie-clicker/main.py
+++ 48-day/cookie-clicker/main.py
@@ -1,7 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import (Service)
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 service = Service("/usr/local/bin/chromedriver")
@@ -10,9 +11,30 @@ driver = webdriver.Chrome(service=service)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID, value="cookie")
+money = driver.find_element(By.ID, "money")
+right_panel = driver.find_elements(By.CLASS_NAME, "amount")
+
+action = ActionChains(driver)
+
+def get_current_money():
+    money_text = money.text
+    return money_text.replace(",","")
+
 
 for i in range(100):
-    cookie.send_keys(Keys)
+
+    action.click(cookie).perform()
+    current_amount = get_current_money()
+
+    for item in right_panel:
+        item_price = item.text.replace(",", "")
+
+        if int(item_price) <= int(current_amount):
+            item.click()
+
+
+
+
 
 driver.quit()
 
