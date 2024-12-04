1ccfbaea61d3d3b4d370656528d978f8b27d6f32
K-Musty <kmustapha9564@gmail.com>
2024-12-04 15:42:18 +0100

    Day 48: Scraping Data for 'money', and 'right panel'

--- 48-day/cookie-clicker/main.py
+++ 48-day/cookie-clicker/main.py
@@ -1,8 +1,10 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import (Service)
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
+
 
 
 service = Service("/usr/local/bin/chromedriver")
@@ -12,29 +14,32 @@ driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 cookie = driver.find_element(By.ID, value="cookie")
 money = driver.find_element(By.ID, "money")
-right_panel = driver.find_elements(By.CLASS_NAME, "amount")
+right_panel = driver.find_elements(By.CSS_SELECTOR, "#store div")
 
-action = ActionChains(driver)
+
 
 def get_current_money():
     money_text = money.text
-    return money_text.replace(",","")
+    return int(money_text.replace(",","")) if money_text.isdigit() else 0
 
 
-for i in range(100):
+time_skip = time.time() + 5
 
-    action.click(cookie).perform()
-    current_amount = get_current_money()
+while True:
 
-    for item in right_panel:
-        item_price = item.text.replace(",", "")
+    cookie.click()
 
-        if int(item_price) <= int(current_amount):
-            item.click()
+    if time.time() > time_skip:
+        current_amount = get_current_money()
 
+        for item in right_panel:
+            item_price = item.text.replace(",", "")
 
+            if int(item_price) <= int(current_amount):
+                item.click()
+                time_skip = time_skip + 5
 
+        time.sleep(0.5)
 
-
-driver.quit()
+    driver.quit()
 
