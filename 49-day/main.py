66173e22084513014c6465546116cd496c6a61d6
K-Musty <kmustapha9564@gmail.com>
2024-12-07 22:01:47 +0100

    Day 49: Loading Selenium

--- 49-day/main.py
+++ 49-day/main.py
@@ -2,7 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 
+
+
 url = ""
+
+
 
 service = Service(url)
 driver = webdriver.Chrome(service=service)
+
+driver.find_element()
+
