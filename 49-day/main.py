9cbab87bf559d69a0e3dfc8fe964f14d3857655d
K-Musty <kmustapha9564@gmail.com>
2024-12-08 22:35:57 +0100

    Day 49: Linkedin login using selenium

--- 49-day/main.py
+++ 49-day/main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+import time
 
 MY_EMAIL = ""
 MY_PASSWORD = ""
