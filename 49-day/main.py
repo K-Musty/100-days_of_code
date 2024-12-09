e16ae98404e58279b0a3dfb5856748b509cbf539
K-Musty <kmustapha9564@gmail.com>
2024-12-09 18:05:49 +0100

    Day 49: Signing in to Linkedin using selenium

--- 49-day/main.py
+++ 49-day/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
 
@@ -15,9 +16,24 @@ url = "https://www.linkedin.com/jobs/search/?currentJobId=4071837289&geoId=10353
 service = Service("/usr/local/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
 
-
-
 driver.get(url)
 
-time.sleep(10)
+time.sleep(5)
+try:
+    sign_in = driver.find_element(By.LINK_TEXT, "Sign in")
+    sign_in.click()
 
+
+    time.sleep(2)
+
+    input_email = driver.find_element(By.ID, "username")
+    input_email.send_keys(MY_EMAIL)
+    input_password = driver.find_element(By.ID, "password")
+    input_password.send_keys(MY_PASSWORD)
+
+    input_password.send_keys(Keys.RETURN)
+
+    time.sleep(10)
+
+finally:
+    driver.quit()
