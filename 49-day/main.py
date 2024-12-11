dc92ba8c17486f0e13aefe8b36c7181a3bc47a15
K-Musty <kmustapha9564@gmail.com>
2024-12-11 23:30:30 +0100

    Day 49: Checking to see if i have a problem with the link so i decided to use 'linkedin.com' to test authentication and login

--- 49-day/main.py
+++ 49-day/main.py
@@ -7,7 +7,9 @@ import time
 MY_EMAIL = ""
 MY_PASSWORD = ""
 
-
+# <button class="sign-in-modal__outlet-btn cursor-pointer btn-md btn-primary btn-secondary" data-tracking-client-ingraph="" data-tracking-control-name="public_jobs_contextual-sign-in-modal_sign-in-modal_outlet-button" data-modal="base-sign-in-modal">
+# <!---->              Sign in
+#           </button>
 url = "https://www.linkedin.com"
 
 
@@ -19,7 +21,7 @@ driver.get(url)
 time.sleep(3)
 
 try:
-    sign_in = driver.find_element(By.XPATH, "/html/body/nav/div/a[2]")
+    sign_in = driver.find_element(By.LINK_TEXT, "Sign in")
 
     sign_in.click()
 
