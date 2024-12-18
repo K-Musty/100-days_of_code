104b58f2939187b26f4e2525b186c401b0cecbe2
K-Musty <kmustapha9564@gmail.com>
2024-12-18 22:36:22 +0100

    Day 50: Login into tinder

--- 50-day/main.py
+++ 50-day/main.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-service = Service("/usr/bin/chromedriver")
+service = Service("/usr/local/bin/chromedriver")
 
-driver = webdriver(service)
+driver = webdriver.Chrome(service=service)
+
+driver.get("https://tinder.com/")
+
+login = driver.find_element(By.LINK_TEXT, "Log in")
+login.click()
+
 
 
