d3e3ee3fdf510f28704177bfaf2e4ab14fe34790
K-Musty <kmustapha9564@gmail.com>
2024-12-26 22:04:03 +0100

    Day 50: Implemtenting Login using selenium

--- 50-day/main.py
+++ 50-day/main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 # from selenium.webdriver.common.keys import Keys
+import time
 
 service = Service("/usr/local/bin/chromedriver")
 
@@ -13,19 +14,24 @@ driver.get("https://tinder.com/")
 EMAIL = ""
 PASSWORD = ""
 
-
+accept = driver.find_element(By.CLASS_NAME, "lxn9zzn")
+accept.click()
 
 login = driver.find_element(By.XPATH, "//*[@id='q2098069830']/div/div[1]/div/div/div/main/div/div[2]/div/div[3]/div/div/button[2]/div[2]/div[2]")
 login.click()
 
-facebook = driver.find_element(By.CLASS_NAME, "Mend(a)")
-facebook.click()
+time.sleep(3)
 
+
+facebook = driver.find_element(By.XPATH, "//*[@id='q369688754']/div/div/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button/div[2]/div[2]/div[2]/div/div")
+facebook.click()
+time.sleep(3)
 email = driver.find_element(By.ID, "email")
 email.send_keys(EMAIL)
 
 password = driver.find_element(By.ID, "pass")
 password.send_keys(PASSWORD)
 password.click()
+
 
 
