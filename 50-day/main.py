95af6c91babb5d729402de34150eff2e1f841a28
K-Musty <kmustapha9564@gmail.com>
2024-12-26 21:47:22 +0100

    Day 50: Implemtenting Login using selenium

--- 50-day/main.py
+++ 50-day/main.py
@@ -2,7 +2,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.common.keys import Keys
 
 service = Service("/usr/local/bin/chromedriver")
 
@@ -15,7 +15,7 @@ PASSWORD = ""
 
 
 
-login = driver.find_element(By.X_PATH, "//*[@id='q2098069830']/div/div[1]/div/div/div/main/div/div[2]/div/div[3]/div/div/button[2]/div[2]/div[2]")
+login = driver.find_element(By.XPATH, "//*[@id='q2098069830']/div/div[1]/div/div/div/main/div/div[2]/div/div[3]/div/div/button[2]/div[2]/div[2]")
 login.click()
 
 facebook = driver.find_element(By.CLASS_NAME, "Mend(a)")
