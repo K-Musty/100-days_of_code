734a2f39ced5b9d5d30d2d995f3e29ae0db2219b
K-Musty <kmustapha9564@gmail.com>
2025-01-01 23:39:27 +0100

    Day 51: Twitter registration and login

--- 51-day/main.py
+++ 51-day/main.py
@@ -1,6 +1,13 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome import webdriver
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-service = Service()
-driver = webdriver.Service(service=service)
+PROMISED_DOWN = 150
+PROMISED_UP = 10
+
+service = Service("usr/local/bin/chromedriver")
+driver = webdriver.Chrome(service=service)
+# Twitter (X) Login
+X_EMAIL = ""
+X_PASSWORD = ""
+driver.get("https://www.x.com")
