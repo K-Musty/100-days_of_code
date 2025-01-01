5d1d6e08b2028de4ce7441a7787c0656d6296421
K-Musty <kmustapha9564@gmail.com>
2025-01-02 00:07:48 +0100

    Day 51: Creating class for InternetSpeedTwitterBot, with attributes and methods

--- 51-day/main.py
+++ 51-day/main.py
@@ -11,3 +11,23 @@ driver = webdriver.Chrome(service=service)
 X_EMAIL = ""
 X_PASSWORD = ""
 driver.get("https://www.x.com")
+
+
+class InternetSpeedTwitterBot:
+
+    def __init__(self, down, up):
+        self.down = down
+        self.up = up
+
+
+    def get_internet_speed(self):
+        pass
+
+
+    def tweet_at_provider(self):
+        pass
+
+
+bot = InternetSpeedTwitterBot(up=PROMISED_UP, down=PROMISED_DOWN)
+internet_speed = bot.get_internet_speed()
+tweet_at = bot.tweet_at_provider()
