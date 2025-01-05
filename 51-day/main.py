43fe2bd1cfaf2d235ee4b147408cce4b1652d961
K-Musty <kmustapha9564@gmail.com>
2025-01-05 23:30:55 +0100

    Day 51: login

--- 51-day/main.py
+++ 51-day/main.py
@@ -51,6 +51,8 @@ class InternetSpeedTwitterBot:
         input_email.send_keys(X_EMAIL)
         input_email.send_keys(Keys.ENTER)
 
+        return f"pass"
+
 
 
 bot = InternetSpeedTwitterBot(service=service_path)
