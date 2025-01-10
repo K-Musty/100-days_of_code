5074c2b72f872b00691e81ac70ef47c19a959847
K-Musty <kmustapha9564@gmail.com>
2025-01-10 21:41:55 +0100

    Day 52: importing webdriver

--- 52-day/main.py
+++ 52-day/main.py
@@ -1,3 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.service import Service
 from selenium.webdriver.by import By
+
+url = ""
+
+service = Service(url)
+
+driver = webdriver.chrome(service=service)
