d0bd499dfd46b231d60c431fad388a10e5ceebdd
K-Musty <kmustapha9564@gmail.com>
2025-01-15 23:57:53 +0100

    Day 52: Correcting inputs and more

--- 52-day/main.py
+++ 52-day/main.py
@@ -1,16 +1,15 @@
 # Instagram Follower Bot🤖 
 from selenium import webdriver
-from selenium.webdriver.service import Service
-from selenium.webdriver.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.key import Key
 
 INSTA_UNAME = ""
 INSTA_PASSWORD = ""
 
-url = ""
-
-service = Service(url)
+service = Service("/usr/local/bin/chromedriver")
 
 driver = webdriver.chrome(service=service)
 driver.findelement()
+
 driver.quit()
