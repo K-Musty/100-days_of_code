11507661f510f6c03e3f85f13d2952c61e2add86
Abdulrahman Kalli Mustapha <126028995+K-Musty@users.noreply.github.com>
2025-01-17 23:35:02 +0100

    Update main.py

--- 52-day/main.py
+++ 52-day/main.py
@@ -10,6 +10,9 @@ INSTA_PASSWORD = ""
 service = Service("/usr/local/bin/chromedriver")
 
 driver = webdriver.chrome(service=service)
+url = ""
+driver.get(url)
+
 driver.find_element(By.XPATH, value="")
 
 driver.quit()
