9dfd985650e238a52b38c3ab6792af873026040f
Abdulrahman Kalli Mustapha <126028995+K-Musty@users.noreply.github.com>
2025-01-23 22:39:09 +0100

    Day 52: importing time

--- 52-day/main.py
+++ 52-day/main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.key import Key
+import time
 
 INSTA_UNAME = ""
 INSTA_PASSWORD = ""
