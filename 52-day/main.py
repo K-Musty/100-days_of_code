8d2b6b3b7075f5ea854b8a9fa92b0ada4b4c4823
K-Musty <kmustapha9564@gmail.com>
2025-01-28 22:34:25 +0100

    Day 52: login with selenium completed

--- 52-day/main.py
+++ 52-day/main.py
@@ -2,12 +2,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.key import Key
+from selenium.webdriver.common.keys import Keys
 import time
 
 INSTA_UNAME = ""
 INSTA_PASSWORD = ""
-url = "https//:www.instagram.com"
+url = "https://www.instagram.com/accounts/login/"
 
 service_path = Service("/usr/local/bin/chromedriver")
 
@@ -18,7 +18,17 @@ class InstaFollower:
         self.driver = webdriver.Chrome(service=service)
 
     def login(self):
-        pass
+        self.driver.get(url=url)
+        time.sleep(5)
+
+        username = self.driver.find_element(By.XPATH, "//*[@id='loginForm']/div[1]/div[1]/div/label/input")
+        username.send_keys(INSTA_UNAME)
+        password_field = self.driver.find_element(By.XPATH, "//*[@id='loginForm']/div[1]/div[2]/div/label/input")
+        password_field.send_keys(INSTA_PASSWORD)
+
+        time.sleep(3)
+        password_field.send_keys(Keys.ENTER)
+
 
     def find_followers(self):
         pass
