22ec0b1b8d534519015d67f43629731d7ce71f14
K-Musty <kmustapha9564@gmail.com>
2025-01-28 23:04:28 +0100

    Day 52: Getting the list of followers

--- 52-day/main.py
+++ 52-day/main.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-INSTA_UNAME = "johnnymont2255"
-INSTA_PASSWORD = "07046989916kallikadi"
+INSTA_UNAME = ""
+INSTA_PASSWORD = ""
 url = "https://www.instagram.com/accounts/login/"
 TARGET_ACCOUNT = ""
 
