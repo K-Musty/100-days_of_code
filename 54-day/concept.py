59c59ae597f49a04d757f6150c3b7193b0431262
K-Musty <kmustapha9564@gmail.com>
2025-02-14 00:33:25 +0100

    Day 54: python decorators

--- 54-day/concept.py
+++ 54-day/concept.py
@@ -1,4 +1,6 @@
 # Python decorators --> The beauty of syntaxic sugar '@'
+import time
+
 
 # Example of decorator function
 
@@ -7,3 +9,22 @@ def decorator_function(function):
         return function
     return wrapper_function()
 
+def delay_function(function):
+
+    def wrapper_function():
+        time.sleep(3)
+        function()
+        function()
+    return wrapper_function
+
+@delay_function
+def say_hello():
+    print("Hello")
+
+def say_bye():
+    print("Bye")
+
+say_hello()
+
+wrap = delay_function(say_bye)
+wrap()
