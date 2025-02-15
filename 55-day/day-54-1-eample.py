71ce5abf1717c0e01314359940d44ae2f3ab248a
K-Musty <kmustapha9564@gmail.com>
2025-02-15 23:59:18 +0100

    Day 55: Advanced decorators with *args and **kwargs

--- 55-day/day-54-1-eample.py
+++ 55-day/day-54-1-eample.py
@@ -1,4 +1,4 @@
-import time
+# import time
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,5 +11,28 @@ def greet():
 def display(abba, num):
     return f"Hello {abba} for {num} everything"
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+def is_authenticator_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper
+
+@is_authenticator_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s Blog post")
+
+
+new_user = User("abba")
+new_user.is_logged_in = True
+create_blog_post(new_user)
+
+
