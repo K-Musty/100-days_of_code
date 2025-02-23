a5ab0c0083e079ac2228131746fda3411e64b4b8
K-Musty <kmustapha9564@gmail.com>
2025-02-23 21:50:30 +0100

    Day 57: Blog Website using Jinja Templating completed

--- 57-day/README.md
+++ 57-day/README.md
@@ -1,2 +1,8 @@
 # DAY 57 of 100 - Templating with Jinja in Flask and building a Blog Website
 ----
+- URL Building and Templating with Jinja in a Flask Application
+
+### The challenge
+
+- Create a blog with Flask and Jinja
+
--- 57-day/blog-templating/main.py
+++ 57-day/blog-templating/main.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template
+import requests
+from post import Post
+
+blog_url = "https://api.npoint.io/25bdf096de4e3edde919"
+response = requests.get(blog_url)
+blog_posts = response.json()
+posts = []
+for i in blog_posts:
+    post = Post(i["id"], i["title"], i["subtitle"], i["body"])
+    posts.append(post)
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts=posts)
+
+@app.route("/blog/<int:num>")
+def blog_post(num):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post.post_id == num:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
