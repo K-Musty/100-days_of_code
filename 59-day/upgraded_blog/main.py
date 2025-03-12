cce4f634ae9822652b75d3a35139cd6fc29ee360
K-Musty <kmustapha9564@gmail.com>
2025-03-12 22:38:50 +0100

    Day 58: Creating Blog Api

--- 59-day/upgraded_blog/main.py
+++ 59-day/upgraded_blog/main.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
+import requests
 
+URL = "https://api.npoint.io/bd5493b8744a52d09b8b"
+
+response = requests.get(url=URL)
+all_posts = response.json()
 app = Flask(__name__)
 
 @app.route("/index.html")
 def home():
-    return render_template("index.html")
+    # for posts in all_posts:
+    #     post_id = posts["id"]
+    #     title = posts["title"]
+    #     subtitle = posts["subtitle"]
+    #     body = posts["body"]
+
+    return render_template("index.html", posts=all_posts)
 
 @app.route("/about.html")
 def about():
