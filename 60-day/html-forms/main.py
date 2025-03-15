b25715dac2e15c33a1ad80f3d742346cfb9d1341
K-Musty <kmustapha9564@gmail.com>
2025-03-15 23:56:24 +0100

    Day 60: Handling form

--- 60-day/html-forms/main.py
+++ 60-day/html-forms/main.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route("/login", methods=["GET", "POST]"])
-def login():
-    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def receive_data():
+    name = request.form.get["username"]
+    password = request.form.get["password"]
+    return f"<h1>Name: {name}, Password: {password}</h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
