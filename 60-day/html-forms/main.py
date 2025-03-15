9a26cff0dcfb7843ef5e7f1ba0f56cf6ad167b64
K-Musty <kmustapha9564@gmail.com>
2025-03-16 00:19:43 +0100

    Day 60: Getting a hold of information with post

--- 60-day/html-forms/main.py
+++ 60-day/html-forms/main.py
@@ -1,18 +1,16 @@
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/login", methods=["POST"])
-def receive_data():
-    name = request.form.get["username"]
-    password = request.form.get["password"]
-    return f"<h1>Name: {name}, Password: {password}</h1>"
-
+def login():
+    name = request.form["username"]
+    password = request.form["password"]
+    return render_template("login.html",name=name, password=password )
 
 if __name__ == "__main__":
     app.run(debug=True)
