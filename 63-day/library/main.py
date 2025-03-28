731e5d7170c90500839a07cce989384c3044f228
K-Musty <kmustapha9564@gmail.com>
2025-03-28 23:59:19 +0100

    Day 63: Still trying to figure out my way around SQLALCHEMY

--- 63-day/library/main.py
+++ 63-day/library/main.py
@@ -4,24 +4,29 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new_books-collections.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collections.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Books(db.Model):
+class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), unique=True, nullable=False)
-    author = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         return f"<Book {self.title}>"
 
+ # Required in newer Flask/SQLAlchemy versions
+with app.app_context():
+    db.create_all()
 
-db.create_all()
-
+    if not Book.query.get(1):
+        new_book = Book(id=1, title="Harry Potter", author="J.K. Rowling", rating=9.3)
+        db.session.add(new_book)
+        db.session.commit()
 #
 # db = sqlite3.connect("books-collections.db")
 # cursor = db.cursor()
