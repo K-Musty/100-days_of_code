18c870791ed9da167b713374ae7b8040d5b9f0b0
K-Musty <kmustapha9564@gmail.com>
2025-04-01 23:33:54 +0100

    Day 63: Practicing more on SQLALCHEMY; made a users table from a class next to authenticate user

--- 63-day/practice/main.py
+++ 63-day/practice/main.py
@@ -1,9 +1,31 @@
 from flask import Flask
+from flask.globals import app_ctx
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///practice_users.db"
 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique=True, nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+    # Create new user
+    if not User.query.get(1):
+
+        new_user = User(id=1, name="Abba", email="abba@abba", password="1234")
+        db.session.add(new_user)
+        db.session.commit()
 
 
 if __name__ == "__main__":
