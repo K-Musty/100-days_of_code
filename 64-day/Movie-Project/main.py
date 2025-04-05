6ff567e68ba89d0d642da22302b883cc169058bb
K-Musty <kmustapha9564@gmail.com>
2025-04-06 00:36:09 +0100

    Day 64: movies database created

--- 64-day/Movie-Project/main.py
+++ 64-day/Movie-Project/main.py
@@ -10,6 +10,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///top_ten_movies.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    title = db.Column(db.String(250), nullable=False, unique=True)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(300), nullable=False)
+    img_url = db.Column(db.String(300), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 
 @app.route("/")
 def home():
