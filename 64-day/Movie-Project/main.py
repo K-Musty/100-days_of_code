7bd9c237314e9e5d74e2409e0696588669fd6b5f
K-Musty <kmustapha9564@gmail.com>
2025-04-07 22:32:23 +0100

    Day 64: Making a movies table class, creating objects using jinja, and displaying the object attributes on the card

--- 64-day/Movie-Project/main.py
+++ 64-day/Movie-Project/main.py
@@ -24,13 +24,28 @@ class Movies(db.Model):
     review = db.Column(db.String(300), nullable=False)
     img_url = db.Column(db.String(300), nullable=False)
 
+
 with app.app_context():
     db.create_all()
 
+    # new_movie = Movies(
+    #     title="Phone Booth",
+    #     year=2002,
+    #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+    #     rating=7.3,
+    #     ranking=10,
+    #     review="My favourite character was the caller.",
+    #     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+    # )
+    #
+    # db.session.add(new_movie)
+    # db.session.commit()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    all_movies = db.session.query(Movies).all()
+    print(all_movies)
+    return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
