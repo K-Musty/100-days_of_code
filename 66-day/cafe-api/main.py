d860246f76c7f68f28907a34d392965374ab7164
K-Musty <kmustapha9564@gmail.com>
2025-04-28 23:26:48 +0100

    Getting Database to retrive information

--- 66-day/cafe-api/main.py
+++ 66-day/cafe-api/main.py
@@ -43,12 +43,22 @@ with app.app_context():
 def home():
     return render_template("index.html")
 
-@app.route("/random", methods=['POST', 'GET'])
+@app.route("/random", methods=["GET"])
 def random_cafe():
-    cafes = db.session.query(Cafe).all()
-    random_cafe = random.choice(cafes)
-
-    return jsonify(cafe=random_cafe.to_dict())
+    with app.app_context():
+        cafes = db.session.execute(db.select(Cafe)).scalars().all()
+        random_cafe = random.choice(cafes)
+        return jsonify(cafe={"can_take_calls":random_cafe.can_take_calls,
+                   "coffee_price":random_cafe.coffee_price,
+                   "has_sockets":random_cafe.has_sockets,
+                   "has_toilet":random_cafe.has_toilet,
+                   "has_wifi":random_cafe.has_wifi,
+                   "id":random_cafe.id,
+                   "img_url":random_cafe.img_url,
+                   "location":random_cafe.location,
+                   "map_url":random_cafe.map_url,
+                   "name":random_cafe.name,
+                   "seats":random_cafe.seats})
 
 @app.route("/all")
 def all_cafe():
