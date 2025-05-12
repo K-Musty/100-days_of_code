b8a45e5ef7cedbdd0eee13839b65fd0f6d27a2d9
K-Musty <kmustapha9564@gmail.com>
2025-05-12 17:30:00 +0100

    Day 67: Blog Capstone Part 3: Building a RESTful Blog with Editing completed

--- 67-day/README.md
+++ 67-day/README.md
@@ -1,1 +1,9 @@
-# DAY 67 of 100
+# DAY 67 of 100 - Blog Capstone Part 3: Building a RESTful Blog with Editing
+----
+
+### Challenges
+- GET Blog post items
+- POST new blog posts - Using CKEditor
+- Edit existing blog posts
+- DELETE blog posts
+
--- 67-day/RESTful-blog/main.py
+++ 67-day/RESTful-blog/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -79,6 +79,38 @@ def make_post():
         db.session.commit()
         return redirect(url_for('get_all_posts'))
     return render_template("make-post.html", form=create_form)
+
+@app.route("/edit-post/<post_id>", methods=['POST', 'GET'])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+@app.route("/delete/<post_id>")
+def remove_blog(post_id):
+    post_to_delete = BlogPost.query.get_or_404(post_id)  # Better error handling
+    try:
+        db.session.delete(post_to_delete)
+        db.session.commit()
+        flash('Post was successfully deleted!', 'success')  # Optional feedback
+    except Exception as e:
+        db.session.rollback()
+        flash('Error deleting post!', 'danger')
+    return redirect(url_for('get_all_posts'))
 
 
 if __name__ == "__main__":
